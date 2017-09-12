93be0ca9f842d3531b40becf51e4110ab38a6ef5
‘NateMietk’ <natemietkiewicz@gmail.com>
2017-09-12 22:26:01 +0000

    cleaned generate summaries sh

--- src/bash/generate_monthly_summaries.sh
+++ src/bash/generate_monthly_summaries.sh
@@ -7,7 +7,13 @@ source metdata_wget.sh
 Rscript --vanilla R/aggregate-climate-data.R
 
 # push monthly GeoTIFFS to AWS S3
-aws s3 cp ../data/processed/pet s3://earthlab-gridmet/pet --recursive
-aws s3 cp ../data/processed/pr s3://earthlab-gridmet/pr --recursive
-aws s3 cp ../data/processed/tmmx s3://earthlab-gridmet/tmmx --recursive
-aws s3 cp ../data/processed/vs s3://earthlab-gridmet/vs --recursive
+aws s3 cp ../data/processed/pet s3://earthlab-natem/data/climate/historical/pet --recursive
+aws s3 cp ../data/processed/pr s3://earthlab-natem/data/climate/historical/pr --recursive
+aws s3 cp ../data/processed/tmmx s3://earthlab-natem/data/climate/historical/tmmx --recursive
+aws s3 cp ../data/processed/tmmn s3://earthlab-natem/data/climate/historical/tmmn --recursive
+aws s3 cp ../data/processed/pdsi s3://earthlab-natem/data/climate/historical/pdsi --recursive
+aws s3 cp ../data/processed/fm100 s3://earthlab-natem/data/climate/historical/fm100 --recursive
+aws s3 cp ../data/processed/fm1000 s3://earthlab-natem/data/climate/historical/fm1000 --recursive
+aws s3 cp ../data/processed/vs s3://earthlab-natem/data/climate/historical/vs --recursive
+aws s3 cp ../data/processed/rmin s3://earthlab-natem/data/climate/historical/rmin --recursive
+aws s3 cp ../data/processed/rmax s3://earthlab-natem/data/climate/historical/rmax --recursive
