47939f209d2436a38e929c28ffaee77f63b1ae0d
‘NateMietk’ <natemietkiewicz@gmail.com>
2017-09-19 18:10:25 +0000

    rasterizing and distance calculations

--- src/R/prepare_data.R
+++ src/R/prepare_data.R
@@ -6,9 +6,10 @@ library(lubridate)
 source("src/R/get_data.R")
 
 # Prepare all spatial data for analysis
-raw_prefix <- file.path("data", "raw")
+raw_prefix <- file.path("../data", "raw")
 
-p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+# p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" # Latlong
+p4string <- "+init=epsg:2163"  # e.g. US National Atlas Equal Area
 
 # Clean the FPA database class
 fpa_fire <- st_read(dsn = file.path(raw_prefix, "fpa-fod", "Data", "FPA_FOD_20170508.gdb"),
@@ -29,16 +30,20 @@ fpa_fire <- st_read(dsn = file.path(raw_prefix, "fpa-fod", "Data", "FPA_FOD_2017
 usa_shp <- st_read(dsn = file.path(raw_prefix, "cb_2016_us_state_20m"),
                    layer = "cb_2016_us_state_20m") %>% 
   st_transform(p4string) %>%
-  filter(!STUSPS %in% c("HI", "AK", "PR"))
+  filter(STUSPS == "RI")
+  #filter(!STUSPS %in% c("HI", "AK", "PR"))
 
 eco_reg <- st_read(dsn = file.path(raw_prefix, "us_eco_l3"),
                    layer = "us_eco_l3") %>% 
-  st_transform(p4string)
+  st_transform(p4string) %>%
+  st_intersection(., usa_shp)
 
 tl <- st_read(dsn = file.path(raw_prefix, "Electric_Power_Transmission_Lines", 'Electric_Power_Transmission_Lines.shp')) %>%
   st_transform(p4string) %>%
   st_intersection(., usa_shp) %>%
   mutate(bool_tl = 1)
+tl <- tl %>% 
+  filter(st_is(., c("LINESTRING"))) 
 
 rails <- st_read(dsn = file.path(raw_prefix, "tlgdb_2015_a_us_rails", 'tlgdb_2015_a_us_rails.gdb'), layer = 'Rails') %>%
   st_transform(p4string) %>%
@@ -57,8 +62,32 @@ pd <- st_read(dsn = file.path(raw_prefix, "county_pop", 'cofips_upp.shp')) %>%
 
 # This will be the raster "template" for all shapefile to raster conversions
 elevation <- raster(file.path(raw_prefix, "metdata_elevationdata", "metdata_elevationdata.nc")) %>%
+  projectRaster(crs = p4string, res = 4000) %>%
+  crop(as(usa_shp, "Spatial")) %>%
   mask(as(usa_shp, "Spatial"))
-projection(elevation) <- CRS(p4string)
+
+
+elevation.disaggregate <- disaggregate(elevation, fact=40) %>%
+  crop(as(usa_shp, "Spatial")) %>%
+  mask(as(usa_shp, "Spatial"))
+
+# Rasterize all shapefiles
+state <- rasterize(as(usa_shp, "Spatial"), elevation)
+ecoregion <- rasterize(as(eco_reg, "Spatial"), elevation)
+transmission_lines_250m <- rasterize(as(tl, "Spatial"), elevation.disaggregate, "bool_tl")
+railroads <- rasterize(as(rails, "Spatial"), elevation, "bool_rail")
+
+transmission_lines_100m <- rasterize(as(tl, "Spatial"), elevation.disaggregate, "bool_tl")
+transmission_lines_4k <- rasterize(as(tl, "Spatial"), elevation, "bool_tl")
+
+tld_100m <- distance(transmission_lines_100m)
+plot(tld_100m)
+tld_4k <- aggregate(tld_100m, fact=40, fun=mean)
+plot(tld_4k)
+
+pop_den <- rasterize(as(pd, "Spatial"), elevation)
+
+roads <- rasterize(as(rds, "Spatial"), elevation, "rds")
 
 # Read in raster data
 hd <- raster(file.path(raw_prefix, "housing_den", 'hd_iclus_bc.nc'))
@@ -66,13 +95,3 @@ hd <- flip(t(hd), direction = "x")
 projection(hd) <- CRS(p4string)
 hd <- crop(hd, elevation)
 hd <- mask(hd, as(usa_shp, "Spatial"))
-
-
-
-# Rasterize all shapefiles
-state <- rasterize(as(usa_shp, "Spatial"), elevation)
-ecoregion <- rasterize(as(eco_reg, "Spatial"), elevation)
-transmission_lines <- rasterize(as(tl, "Spatial"), elevation, "bool_tl")
-railroads <- rasterize(as(rails, "Spatial"), elevation, "bool_rail")
-roads <- rasterize(as(rds, "Spatial"), elevation, "rds")
-pop_den <- rasterize(as(pd, "Spatial"), elevation)
