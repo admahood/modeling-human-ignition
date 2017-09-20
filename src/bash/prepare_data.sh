af69973259aa488e9562b708265a84b19104d01c
Nathan Mietkiewicz <natemietkiewicz@gmail.com>
2017-09-20 14:38:40 -0600

    Update prepare_data.sh

--- src/bash/prepare_data.sh
+++ src/bash/prepare_data.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Use the get_data.R file if these are not available on S3
+# Rscript --vanilla ../HumanIgnProb/src/R/get_data.R
+
+# get ancillary data from S3
+aws s3 sync s3://earthlab-natem/data/raw ../data/raw
+
 # execute R script to rasterize shapefiles
 Rscript --vanilla ../HumanIgnProb/src/R/prepare_data.R
 
 # get ancillary data from S3
-aws s3 cp ../data/processed s3://earthlab-natem/data/processed --recursive
+aws s3 sync ../data/processed s3://earthlab-natem/data/processed
