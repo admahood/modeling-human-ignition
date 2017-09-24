71d34aa443be03b71f85199b4ff6f7dcc102a73f
‘NateMietk’ <natemietkiewicz@gmail.com>
2017-09-24 23:08:22 +0000

    update rstudio project in path

--- src/bash/generate_monthly_summaries.sh
+++ src/bash/generate_monthly_summaries.sh
@@ -4,7 +4,7 @@
 source src/bash/metdata_wget.sh
 
 # execute R script to produce monthly GeoTIFFs
-Rscript --vanilla ../HumanIgnProb/src/R/aggregate_climate_data.R
+Rscript --vanilla ../modeling-human-ignition/src/R/aggregate_climate_data.R
 
 # push monthly GeoTIFFS to AWS S3
 aws s3 sync ../data/processed/ s3://earthlab-natem/data/climate/historical/
