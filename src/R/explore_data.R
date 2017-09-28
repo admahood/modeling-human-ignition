4f57a0621efa837c0cd7b8f35ad889151c427556
NateMietk <natemietkiewicz@gmail.com>
2017-09-28 12:23:40 -0600

    added more exploring figures

--- src/R/explore_data.R
+++ src/R/explore_data.R
@@ -1,8 +1,5 @@
-library(raster)
-library(tidyverse)
-library(sf)
-library(lubridate)
-library(snowfall)
+x <- c("raster", "tidyverse", "sf", "rasterVis", "gridExtra")
+lapply(x, library, character.only = TRUE, verbose = FALSE)
 
 raw_prefix <- ifelse(Sys.getenv("LOGNAME") == "NateM", file.path("data", "raw"), 
                      ifelse(Sys.getenv("LOGNAME") == "nami1114", file.path("data", "raw"), 
@@ -24,6 +21,10 @@ ecoreg <- st_read(dsn = file.path(raw_prefix, "us_eco_l3"),
                   layer = "us_eco_l3", quiet= TRUE) %>%
   st_transform(p4string_ea) %>%
   st_simplify(., preserveTopology = TRUE, dTolerance = 1000)
+
+eco_reg_1 <- ecoreg %>%
+  group_by(NA_L1NAME) %>%
+  summarise()
 
 ecoreg_names <- ecoreg %>%
   as.data.frame(.) %>%
@@ -58,34 +59,47 @@ rst_df <-  rst_df %>%
 
 p1 <- rst_df %>%
   ggplot(aes(x = dis_primary_rds/1000, y = fpa_density)) +
-  geom_point(aes(color = region), alpha = 0.5, shape = 16) + theme_pub() +
+  geom_point(aes(color = region), alpha = 0.10, shape = 16) + theme_pub() +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
   theme(legend.position = "none") +
-  xlab("Distance to Primary Roads (km)") + ylab("Human ignition wildfire density per 4km")
+  xlab("Distance to Primary Roads (km)") + 
+  ylab("Human ignition wildfire density per 4km")
 
 p2 <- rst_df %>%
+  ggplot(aes(x = dis_secondary_rds/1000, y = fpa_density)) +
+  geom_point(aes(color = region), alpha = 0.10, shape = 16) + theme_pub() +
+  theme(legend.position = "none") +
+  scale_color_manual(values = c("#1F77B4", "#D62728")) +
+  xlab("Distance to Secondary Roads (km)") + ylab("")
+
+p3 <- rst_df %>%
+  ggplot(aes(x = dis_all_rds/1000, y = fpa_density)) +
+  geom_point(aes(color = region), alpha = 0.10, shape = 16) + theme_pub() +
+  scale_color_manual(values = c("#1F77B4", "#D62728")) +
+  theme(legend.position = "none") +
+  xlab("Distance to All Roads (km)") + ylab("") 
+
+p4 <- rst_df %>%
   ggplot(aes(x = dis_transmission_lines/1000, y = fpa_density)) +
-  geom_point(aes(color = region), alpha = 0.5, shape = 16) + theme_pub() +
+  geom_point(aes(color = region), alpha = 0.10, shape = 16) + theme_pub() +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
   xlab("Distance to Transmission Lines (km)") + ylab("")
 
-p3 <- rst_df %>%
+p5 <- rst_df %>%
   ggplot(aes(x = dis_railroads/1000, y = fpa_density)) +
-  geom_point(aes(color = region, size), alpha = 0.5, shape = 16) + theme_pub() +
+  geom_point(aes(color = region), alpha = 0.10, shape = 16) + theme_pub() +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
   theme(legend.position = "none") +
   xlab("Distance to Railroads (km)") + ylab("Human ignition wildfire density per 4km")
 
-p4 <- rst_df %>%
+p6 <- rst_df %>%
   ggplot(aes(x = elevation, y = fpa_density)) +
-  geom_point(aes(color = region), alpha = 0.5, shape = 16) + theme_pub() +
+  geom_point(aes(color = region), alpha = 0.10, shape = 16) + theme_pub() +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
   xlab("Elevation (m)") + ylab("")
 
-library(gridExtra)
-
-grid.arrange(p1, p2, p3, p4, nrow = 2)
-g <- arrangeGrob(p1, p2, p3, p4, nrow = 2)
+#grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 3)
+g <- arrangeGrob(p1, p2, p3, p4, p5, p6, nrow = 2)
 ggsave(file = "results/fireden_per_var.png", g, width = 8, height = 6, dpi=600, units = "cm", scale = 3) #saves g
 
 
@@ -97,37 +111,31 @@ p1 <- rst_df %>%
   xlab("Distance to Primary Roads (km)") + ylab("Human ignition wildfire density (4km)") +
   facet_wrap(~NA_L1NAME, labeller = label_wrap_gen(10),
              scales = "free")
-ggsave(file = "results/fireden_prirds_ecoreg.png", p1, width = 6, 
+ggsave(file = "results/fireden_prirds_ecoreg.jpg", p1, width = 6, 
        height = 8, dpi=600, units = "cm", scale = 3) #saves g
 
 p2 <- rst_df %>%
   ggplot(aes(x = dis_secondary_rds/1000, y = fpa_density)) +
   geom_point(alpha = 0.5, shape = 16) + theme_pub() +
   scale_color_manual(values = "#D62728") +
-  xlab("Distance to Secondary Roads (km)") + ylab("Human ignition wildfire density (4km)") +
-  facet_wrap(~NA_L1NAME, labeller = labeller(groupwrap = label_wrap_gen(10)),
-             scales = "free")
-ggsave(file = "results/fireden_secrds_ecoreg.png", p2, width = 6, 
+  xlab("Distance to Secondary Roads (km)") + ylab("Human ignition wildfire density (4km)") 
+ggsave(file = "results/fireden_secrds_ecoreg.jpg", p2, width = 6, 
        height = 8, dpi=600, units = "cm", scale = 3) #saves g
 
 p3 <- rst_df %>%
   ggplot(aes(x = dis_all_rds/1000, y = fpa_density)) +
   geom_point(alpha = 0.5, shape = 16) + theme_pub() +
   scale_color_manual(values = "#D62728") +
-  xlab("Distance to All Roads (km)") + ylab("Human ignition wildfire density (4km)") +
-  facet_wrap(~NA_L1NAME, labeller = labeller(groupwrap = label_wrap_gen(10)),
-             scales = "free")
-ggsave(file = "results/fireden_allrds_ecoreg.png", p3, width = 6, 
+  xlab("Distance to All Roads (km)") + ylab("Human ignition wildfire density (4km)")
+ggsave(file = "results/fireden_allrds_ecoreg.jpg", p3, width = 6, 
        height = 8, dpi=600, units = "cm", scale = 3) #saves g
 
 p4 <- rst_df %>%
   ggplot(aes(x = dis_transmission_lines/1000, y = fpa_density)) +
   geom_point(alpha = 0.5, shape = 16) + theme_pub() +
   scale_color_manual(values = "#D62728") +
-  xlab("Distance to Transmission Lines (km)") + ylab("Human ignition wildfire density (4km)") +
-  facet_wrap(~NA_L1NAME, labeller = labeller(groupwrap = label_wrap_gen(10)),
-             scales = "free")
-ggsave(file = "results/fireden_translines_ecoreg.png", p4, width = 6, 
+  xlab("Distance to Transmission Lines (km)") + ylab("Human ignition wildfire density (4km)")
+ggsave(file = "results/fireden_translines_ecoreg.jpg", p4, width = 6, 
        height = 8, dpi=600, units = "cm", scale = 3) #saves g
 
 p5 <- rst_df %>%
@@ -136,9 +144,9 @@ p5 <- rst_df %>%
   scale_color_manual(values = "#D62728") +
   theme(legend.position = "none") +
   xlab("Distance to Railroads (km)") + ylab("Human ignition wildfire density (4km)") +
-  facet_wrap(~NA_L1NAME, labeller = labeller(groupwrap = label_wrap_gen(10)),
+  facet_wrap(~NA_L1NAME, labeller = label_wrap_gen(10),
              scales = "free")
-ggsave(file = "results/fireden_railrds_ecoreg.png", p5, width = 6, 
+ggsave(file = "results/fireden_railrds_ecoreg.jpg", p5, width = 6, 
        height = 8, dpi=600, units = "cm", scale = 3) #saves g
 
 p6 <- rst_df %>%
@@ -146,8 +154,45 @@ p6 <- rst_df %>%
   geom_point(alpha = 0.5, shape = 16) + theme_pub() +
   scale_color_manual(values = "#D62728") +
   xlab("Elevation (m)") + ylab("Human ignition wildfire density (4km)") +
-  facet_wrap(~NA_L1NAME, labeller = labeller(groupwrap = label_wrap_gen(10)),
+  facet_wrap(~NA_L1NAME, labeller = label_wrap_gen(10),
              scales = "free")
-ggsave(file = "results/fireden_elevation_ecoreg.png", p6, width = 6, 
+ggsave(file = "results/fireden_elevation_ecoreg.jpg", p6, width = 6, 
        height = 8, dpi=600, units = "cm", scale = 3) #saves g
+
+
+jpeg("results/anthro_var_maps/fpa_density.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[1]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
+jpeg("results/anthro_var_maps/primary_roads.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[2]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
+jpeg("results/anthro_var_maps/secondary_roads.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[3]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
+jpeg("results/anthro_var_maps/all_roads.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[4]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
+jpeg("results/anthro_var_maps/rail_roads.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[5]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
+jpeg("results/anthro_var_maps/tranmission_lines.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[6]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
+jpeg("results/anthro_var_maps/elevation.jpeg", width=500, height=700, quality = 100)  #0.707 is a convenient aspect.ratio
+p <- levelplot(rsts[[7]], layers = 1, margin = list(FUN = 'median'), par.settings = RdBuTheme)
+p + layer(sp.polygons(as(eco_reg_1, "Spatial"), lwd=0.8, col='black'))
+dev.off()
+
 
