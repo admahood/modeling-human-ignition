4f624e14125c89e06460e34057c3166016ec687b
NateMietk <natemietkiewicz@gmail.com>
2017-09-28 12:22:38 -0600

    added ffwi check from transform - much more efficent

--- src/R/aggregate_climate_data.R
+++ src/R/aggregate_climate_data.R
@@ -5,7 +5,7 @@ library(rgdal)
 library(tidyverse)
 library(assertthat)
 library(snowfall)
-
+library(ncdf4)
 # Creat directories for state data
 raw_prefix <- ifelse(Sys.getenv("LOGNAME") == "NateM", file.path("data", "raw"), 
                      ifelse(Sys.getenv("LOGNAME") == "nami1114", file.path("data", "raw"), 
@@ -27,7 +27,7 @@ for(i in rep(1979:2016)) {
   }
 }
 
-us_shp <- file.path("../data", "raw", "cb_2016_us_state_20m", "cb_2016_us_state_20m.shp")
+us_shp <- file.path(raw_prefix, "cb_2016_us_state_20m", "cb_2016_us_state_20m.shp")
 if (!file.exists(us_shp)) {
   loc <- "https://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip"
   dest <- paste0(us_prefix, ".zip")
@@ -36,15 +36,14 @@ if (!file.exists(us_shp)) {
   unlink(dest)
 }
 
-usa_shp <- readOGR(dsn = file.path("../data/raw/cb_2016_us_state_20m/"),
+usa_shp <- readOGR(dsn = file.path(raw_prefix, "cb_2016_us_state_20m/"),
                    layer = "cb_2016_us_state_20m")
 
 usa_shp <- spTransform(usa_shp,
-                       CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+                       CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
 
 daily_to_monthly <- function(file, mask){
-
   x <- c("lubridate", "rgdal", "ncdf4", "raster", "tidyverse", "snowfall")
   lapply(x, require, character.only = TRUE)
   
@@ -66,7 +65,7 @@ daily_to_monthly <- function(file, mask){
   dir_sum_95thpct <- file.path(data_var, "sum_days_95thpct")
   
   var_dir <- list(data_pro, data_var, dir_mean, dir_std,
-                  dir_sum_90thpct, dir_sum_95thpct)
+                  dir_90th, dir_95th, dir_sum_90thpct, dir_sum_95thpct)
   
   lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
   
@@ -76,7 +75,11 @@ daily_to_monthly <- function(file, mask){
     array <- aperm(array, c(3, 2, 1))
     raster <- brick(array, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs",
                     xmn = -124.793, xmx = -67.043,  ymn = 25.04186, ymx = 49.41686)
-  } else{ raster <- brick(file) }
+  } else{ 
+    raster <- brick(file) 
+    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+    projection(raster) <- CRS(p4string)
+  }
   
   start_date <- as.Date(paste(year, "01", "01", sep = "-"))
   end_date <- as.Date(paste(year, "12", "31", sep = "-"))
@@ -88,12 +91,12 @@ daily_to_monthly <- function(file, mask){
   # Mean
   if(!file.exists(file.path(dir_mean, paste0(var, "_", year, "_mean",".tif")))) {
     monthly_mean <- stackApply(raster, month_seq, fun = mean)
-    monthly_mean <- flip(t(monthly_mean), direction = "x")
+    if(var != "ffwi") {
+      monthly_mean <- flip(t(monthly_mean), direction = "x") 
+      }
     names(monthly_mean) <- paste(var, year,
                                  unique(month(date_seq, label = TRUE)),
                                  sep = "_")
-    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    projection(monthly_mean) <- CRS(p4string)
     monthly_mean <- mask(monthly_mean, mask)
     writeRaster(monthly_mean, filename = file.path(dir_mean, paste0(var, "_", year, "_mean",".tif")),
                 format = "GTiff") 
@@ -102,12 +105,12 @@ daily_to_monthly <- function(file, mask){
   # Standard deviation
   if(!file.exists(file.path(dir_std, paste0(var, "_", year, "_std",".tif")))){
     monthly_std <- stackApply(raster, month_seq, fun = sd)
-    monthly_std <- flip(t(monthly_std), direction = "x")
+    if(var != "ffwi") {
+      monthly_std <- flip(t(monthly_std), direction = "x") 
+    }
     names(monthly_std) <- paste(var, year,
                                 unique(month(date_seq, label = TRUE)),
                                 sep = "_")
-    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    projection(monthly_std) <- CRS(p4string)
     monthly_std <- mask(monthly_std, mask)
     writeRaster(monthly_std, filename = file.path(dir_std, paste0(var, "_", year, "_std",".tif")),
                 format = "GTiff") 
@@ -123,13 +126,12 @@ daily_to_monthly <- function(file, mask){
                      fun = function(x) raster::quantile(x, probs = 0.90, na.rm = T))
       mean_90thpct <- stack(mean_90thpct, pctile)
     }
-    
-    mean_90thpct <- flip(t(mean_90thpct), direction = "x")
+    if(var != "ffwi") {
+      mean_90thpct <- flip(t(mean_90thpct), direction = "x") 
+    }
     names(mean_90thpct) <- paste(var, year,
                                  unique(month(date_seq, label = TRUE)),
                                  sep = "_")
-    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    projection(mean_90thpct) <- CRS(p4string)
     mean_90thpct <- mask(mean_90thpct, mask)
     writeRaster(mean_90thpct, filename = file.path(dir_90th, paste0(var, "_", year, "_90th",".tif")),
                 format = "GTiff") 
@@ -145,12 +147,12 @@ daily_to_monthly <- function(file, mask){
                      fun = function(x) raster::quantile(x, probs = 0.95, na.rm = T))
       mean_95thpct <- stack(mean_95thpct, pctile)
     }
-    mean_95thpct <- flip(t(mean_95thpct), direction = "x")
+    if(var != "ffwi") {
+      mean_95thpct <- flip(t(mean_95thpct), direction = "x") 
+    }
     names(mean_95thpct) <- paste(var, year,
                                  unique(month(date_seq, label = TRUE)),
                                  sep = "_")
-    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    projection(mean_95thpct) <- CRS(p4string)
     mean_95thpct <- mask(mean_95thpct, mask)
     writeRaster(mean_95thpct, filename = file.path(dir_95th, paste0(var, "_", year, "_95th",".tif")),
                 format = "GTiff") 
@@ -160,12 +162,12 @@ daily_to_monthly <- function(file, mask){
   if(!file.exists(file.path(dir_sum_90thpct, paste0(var, "_", year, "_numdays90th",".tif")))){
     sum_days_90thpct <- calc(raster, fun = function(x){x > quantile(x, probs = 0.90, na.rm =TRUE)})
     sum_days_90thpct <- stackApply(sum_days_90thpct, month_seq, fun = mean)
-    corrected_res_mean <- flip(t(sum_days_90thpct), direction = "x")
+    if(var != "ffwi") {
+      sum_days_90thpct <- flip(t(sum_days_90thpct), direction = "x") 
+    }
     names(sum_days_90thpct) <- paste(var, year,
                                      unique(month(date_seq, label = TRUE)),
                                      sep = "_")
-    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    projection(sum_days_90thpct) <- CRS(p4string)
     sum_days_90thpct <- mask(sum_days_90thpct, mask)
     writeRaster(sum_days_90thpct, filename = file.path(dir_sum_90thpct, paste0(var, "_", year, "_numdays90th",".tif")),
                 format = "GTiff") 
@@ -175,12 +177,12 @@ daily_to_monthly <- function(file, mask){
   if(!file.exists(file.path(dir_sum_95thpct, paste0(var, "_", year, "_numdays95th",".tif")))){
     sum_days_95thpct <- calc(raster, fun = function(x){x > quantile(x, probs = 0.95, na.rm =TRUE)})
     sum_days_95thpct <- stackApply(sum_days_95thpct, month_seq, fun = sum)
-    sum_days_95thpct <- flip(t(sum_days_95thpct), direction = "x")
+    if(var != "ffwi") {
+      sum_days_95thpct <- flip(t(sum_days_95thpct), direction = "x") 
+    }
     names(sum_days_95thpct) <- paste(var, year,
                                      unique(month(date_seq, label = TRUE)),
                                      sep = "_")
-    p4string <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-    projection(sum_days_95thpct) <- CRS(p4string)
     masked_res_mean <- mask(sum_days_95thpct, mask)
     writeRaster(sum_days_95thpct, filename = file.path(dir_sum_95thpct, paste0(var, "_", year, "_numdays95th",".tif")),
                 format = "GTiff") 
