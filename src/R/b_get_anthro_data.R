9c6bf2052874cf3c3b1cd4c9d770a7add03ccb1c
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-02-01 14:14:49 -0700

    removed the ecoregions and usa download

--- src/R/b_get_anthro_data.R
+++ src/R/b_get_anthro_data.R
@@ -1,29 +1,5 @@
 source("src/R/a_make_dirs.R")
 
-#Download the USA States layer
-
-us_shp <- file.path(us_prefix, "cb_2016_us_state_20m.shp")
-if (!file.exists(us_shp)) {
-  loc <- "https://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip"
-  dest <- paste0(us_prefix, ".zip")
-  download.file(loc, dest)
-  unzip(dest, exdir = us_prefix)
-  unlink(dest)
-  assert_that(file.exists(us_shp))
-}
-
-#Download the Level 3 Ecoregions
-
-ecoregion_shp <- file.path(ecoregion_prefix, "us_eco_l3.shp")
-if (!file.exists(ecoregion_shp)) {
-  loc <- "ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/us_eco_l3.zip"
-  dest <- paste0(ecoregion_prefix, ".zip")
-  download.file(loc, dest)
-  unzip(dest, exdir = ecoregion_prefix)
-  unlink(dest)
-  assert_that(file.exists(ecoregion_shp))
-}
-
 # Download the FPA-FOD data
 
 fpa_gdb <- file.path(fpa_prefix, "Data", "FPA_FOD_20170508.gdb")
