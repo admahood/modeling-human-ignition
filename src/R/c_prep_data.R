c6808493946e22898a89f5215ff81ff112847c87
‘admahood’ <admahood@gmail.com>
2018-02-20 22:18:19 +0000

    a, b, and c are all working (mostly)

--- src/R/c_prep_data.R
+++ src/R/c_prep_data.R
@@ -1,54 +1,90 @@
 source("src/functions/download-data.R")
 
+s3_anc_prefix <- 's3://earthlab-modeling-human-ignitions/ancillary/'
+fishnet_path <- "data/ancillary/fishnet"
+fpa_clean_path <- "data/processed/"
+s3_proc_prefix <- 's3://earthlab-modeling-human-ignitions/processed/'
+
 # Download and import the Level 3 Ecoregions data
 # Download will only happen once as long as the file exists
-ecoregions <- load_data(url = "ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/us_eco_l3.zip",
-                        dir = ecoregion_prefix, layer = "us_eco_l3", outname = "ecoregion") %>%
-  st_simplify(., preserveTopology = TRUE, dTolerance = 1000)  %>%
-  mutate(NA_L3NAME = as.character(NA_L3NAME),
-         NA_L3NAME = ifelse(NA_L3NAME == 'Chihuahuan Desert',
-                            'Chihuahuan Deserts',
-                            NA_L3NAME))
+if (!exists("ecoregions")){
+  ecoregions <- load_data(url = "ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/us_eco_l3.zip",
+                          dir = ecoregion_prefix, layer = "us_eco_l3", outname = "ecoregion") %>%
+    st_simplify(., preserveTopology = TRUE, dTolerance = 1000)  %>%
+    mutate(NA_L3NAME = as.character(NA_L3NAME),
+           NA_L3NAME = ifelse(NA_L3NAME == 'Chihuahuan Desert',
+                              'Chihuahuan Deserts',
+                              NA_L3NAME))
+}
 
 # Download and import CONUS states
 # Download will only happen once as long as the file exists
-usa_shp <- load_data(url = "https://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip",
-                    dir = us_prefix,
-                    layer = "cb_2016_us_state_20m",
-                    outname = "usa") %>%
-  st_transform(st_crs(ecoregions)) %>%
-  filter(!STUSPS %in% c("HI", "AK", "PR"))
+if (!exists("usa_shp")){
+  usa_shp <- load_data(url = "https://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip",
+                      dir = us_prefix,
+                      layer = "cb_2016_us_state_20m",
+                      outname = "usa") %>%
+    st_transform(st_crs(ecoregions)) %>%
+    filter(!STUSPS %in% c("HI", "AK", "PR"))
+}
 
 # Create raster mask
 # 4k Fishnet
 if (!exists("fishnet_4k")) {
- # if (!file.exists(FLINEMAE)) do an if else, make fishnet dir... ancillary do this for fpa too
+ if (file.exists(file.path(fishnet_path, "fishnet_4k.gpkg"))) {
+   fishnet_4k <- st_read(file.path(fishnet_path, "fishnet_4k.gpkg"))
+   }
+  
+  else{
   fishnet_4k <- st_make_grid(usa_shp, cellsize = 4000, what = 'polygons') %>%
   st_sf('geometry' = ., data.frame('fishid4k' = 1:length(.))) %>%
   st_intersection(., st_union(usa_shp))
+  
+  st_write(fishnet_4k,
+           file.path(fishnet_path, "fishnet_4k.gpkg"),
+           driver = "GPKG")
+  system(paste0("aws s3 cp ",
+                fishnet_path, "/fishnet_4k.gpkg ",
+                s3_anc_prefix, "fishnet/fishnet_4k.gpkg"))
+ }
 }
 
 # Load and process FPA-FOD wildfire iginition data
-if (!exists("fpa")) {
-fpa <- st_read(dsn = file.path(fpa_prefix, "Data", "FPA_FOD_20170508.gdb"),
-                    layer = "Fires", quiet= FALSE) %>%
-  st_transform(st_crs(ecoregions)) %>%
-  st_intersection(., usa_shp)
-}
+
 
 if (!exists("fpa_clean")) {
-  fpa_clean <- fpa %>%
-  filter(FIRE_SIZE >= 1 & STAT_CAUSE_DESCR != "HUMAN") %>%
-  dplyr::select(FPA_ID, LATITUDE, LONGITUDE, ICS_209_INCIDENT_NUMBER, ICS_209_NAME, MTBS_ID, MTBS_FIRE_NAME,
-                FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, STAT_CAUSE_DESCR, FIRE_SIZE, STATE)  %>%
-  mutate(cause = ifelse(STAT_CAUSE_DESCR == "Lightning", "Lightning", "Human"),
-         FIRE_SIZE_km2 = (FIRE_SIZE*4046.86)/1000000,
-         doy = day(DISCOVERY_DATE),
-         month = month(DISCOVERY_DATE),
-         year = FIRE_YEAR,
-         ym = as.yearmon(paste(year, sprintf("%02d", month),
-                               sep = "-"))) %>%
-  st_join(., fishnet_4k, join = st_intersects)
+  if (file.exists(file.path(fpa_clean_path, "fpa_clean.gpkg"))){
+    fpa_clean <- st_read(file.path(fpa_clean_path, "fpa_clean.gpkg"))
+    st_join(., fishnet_4k, join = st_intersects)
+  }
+  else{
+    if (!exists("fpa")) {
+      fpa <- st_read(dsn = file.path(fpa_prefix, "Data", "FPA_FOD_20170508.gdb"),
+                     layer = "Fires", quiet= FALSE) %>%
+        st_transform(st_crs(ecoregions)) %>%
+        st_intersection(., usa_shp)
+    }
+    
+    fpa_clean <- fpa %>%
+    filter(FIRE_SIZE >= 1 & STAT_CAUSE_DESCR != "HUMAN") %>%
+    dplyr::select(FPA_ID, LATITUDE, LONGITUDE, ICS_209_INCIDENT_NUMBER, ICS_209_NAME, MTBS_ID, MTBS_FIRE_NAME,
+                  FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, STAT_CAUSE_DESCR, FIRE_SIZE, STATE)  %>%
+    mutate(cause = ifelse(STAT_CAUSE_DESCR == "Lightning", "Lightning", "Human"),
+           FIRE_SIZE_km2 = (FIRE_SIZE*4046.86)/1000000,
+           doy = day(DISCOVERY_DATE),
+           month = month(DISCOVERY_DATE),
+           year = FIRE_YEAR,
+           ym = as.yearmon(paste(year, sprintf("%02d", month),
+                                 sep = "-"))) %>%
+    st_join(., fishnet_4k, join = st_intersects)
+    
+    st_write(fpa_clean,
+             file.path(fpa_clean_path, "fpa_clean.gpkg"),
+             driver = "GPKG")
+    system(paste0("aws s3 cp ",
+                  fpa_clean_path, "/fpa_clean.gpkg ",
+                  s3_proc_prefix, "fpa_clean.gpkg"))
+  }
 }
 
 # count the number of human and lightning fires in each pixel in each month for each year
@@ -64,24 +100,27 @@ count_df <- fpa_clean %>%
 
 assert_that(0 == sum(is.na(count_df$fishid4k)))
 assert_that(sum(count_df$n_fire) == nrow(fpa_clean))
-assert_that(all(fishnet_4k$fishid4k %in% count_df$fishid4k))
+assert_that(all(fishnet_4k$fishid4k %in% count_df$fishid4k)) 
+# last line is throwing back errors...
+
 
 #### This DOESNT WORK AS OF 2/7/2018 - still compiling the summaries table!#################
 # load covariate data and link to count data frame
-ecoregion_summaries <- read_csv(<add url here>,
-                                col_types = cols(
-                                  NA_L3NAME = col_character(),
-                                  variable = col_character(),
-                                  year = col_number(),
-                                  month = col_number(),
-                                  wmean = col_number())
-) %>%
-  mutate(year = ifelse(year == 2, 2000, year),
-         year = parse_number(year),
-         ym = as.yearmon(paste(year,
-                               sprintf("%02d", month),
-                               sep = "-"))) %>%
-  spread(variable, wmean)
+
+# ecoregion_summaries <- read_csv(<add url here>,
+#                                 col_types = cols(
+#                                   NA_L3NAME = col_character(),
+#                                   variable = col_character(),
+#                                   year = col_number(),
+#                                   month = col_number(),
+#                                   wmean = col_number())
+# ) %>%
+#   mutate(year = ifelse(year == 2, 2000, year),
+#          year = parse_number(year),
+#          ym = as.yearmon(paste(year,
+#                                sprintf("%02d", month),
+#                                sep = "-"))) %>%
+#   spread(variable, wmean)
 
 # Import ancillary data
 # Roads
