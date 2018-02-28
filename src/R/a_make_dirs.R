32b5ef48f37568e7c02753fdd99cb865cdf107aa
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-02-28 13:20:31 -0700

    added procesing and S3 dirs

--- src/R/a_make_dirs.R
+++ src/R/a_make_dirs.R
@@ -5,6 +5,8 @@ lapply(x, library, character.only = TRUE, verbose = FALSE)
 
 source('src/functions/helper_functions.R')
 
+setwd("/home/rstudio/modeling-human-ignition/")
+
 # Projections
 p4string_ed <- "+proj=eqdc +lat_0=0 +lon_0=0 +lat_1=33 +lat_2=45 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"   #http://spatialreference.org/ref/esri/102005/
 p4string_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"   #http://spatialreference.org/ref/sr-org/6903/
@@ -13,6 +15,7 @@ ncor <- parallel::detectCores()
 
 prefix <- ("data")
 raw_prefix <- file.path(prefix, "raw")
+processed <- file.path(prefix, "processed")
 us_prefix <- file.path(raw_prefix, "cb_2016_us_state_20m")
 
 ecoregion_prefix <- file.path(raw_prefix, "us_eco_l3")
@@ -27,13 +30,19 @@ tl_prefix <- file.path(raw_prefix, 'Electric_Power_Transmission_Lines')
 climate_prefix <- file.path(raw_prefix, "climate")
 
 ancillary_dir <- file.path(prefix, "ancillary")
-fire_dir <- file.path(ancillary_dir, "fire")
-anthro_dir <- file.path(ancillary_dir, "anthro")
+processed_dir <- file.path(prefix, 'processed')
+anthro_dir <- file.path(prefix, "anthro")
+
+fire_dir <- file.path(processed_dir, "fire")
+fishnet_path <- file.path(ancillary_dir, "fishnet")
+
+s3_anc_prefix <- 's3://earthlab-modeling-human-ignitions/ancillary/'
+s3_proc_prefix <- 's3://earthlab-modeling-human-ignitions/processed/'
 
 # Check if directory exists for all variable aggregate outputs, if not then create
 var_dir <- list(prefix, raw_prefix, us_prefix, ecoregion_prefix, roads_prefix,
                 fpa_prefix, rails_prefix, pd_prefix, iclus_prefix, climate_prefix,
-                nlcd_prefix, elev_prefix, tl_prefix, ancillary_dir, fire_dir, anthro_dir)
+                nlcd_prefix, elev_prefix, tl_prefix, ancillary_dir, fire_dir, anthro_dir,
+                fishnet_path, processed_dir)
 
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
-
