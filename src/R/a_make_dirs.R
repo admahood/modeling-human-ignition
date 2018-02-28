e65aee1009919872d0efa2efda9152df00e4f4ab
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-02-28 14:18:12 -0700

    removed setwd

--- src/R/a_make_dirs.R
+++ src/R/a_make_dirs.R
@@ -5,8 +5,6 @@ lapply(x, library, character.only = TRUE, verbose = FALSE)
 
 source('src/functions/helper_functions.R')
 
-setwd("/home/rstudio/modeling-human-ignition/")
-
 # Projections
 p4string_ed <- "+proj=eqdc +lat_0=0 +lon_0=0 +lat_1=33 +lat_2=45 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"   #http://spatialreference.org/ref/esri/102005/
 p4string_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"   #http://spatialreference.org/ref/sr-org/6903/
