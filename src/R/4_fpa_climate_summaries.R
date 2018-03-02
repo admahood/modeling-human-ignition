c219faaf2e9771518bf15ddf36ce3bd1f9c00c6f
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-02 15:39:42 -0700

    added tryCatch and additional checks to skip varaibles that are not present in the list.files

--- src/R/4_fpa_climate_summaries.R
+++ src/R/4_fpa_climate_summaries.R
@@ -1,10 +1,10 @@
 
 if (!exists("fpa_ll")) {
-  
+
   fpa_ll <- st_read(file.path(processed_dir, "fpa_clean.gpkg")) %>%
-    st_par(., st_transform, n_cores = ncor, y = st_crs(usa_shp)) %>%
+    st_par(., st_transform, n_cores = ncor, crs = st_crs(usa_shp)) %>%
     st_intersection(., st_union(usa_shp)) %>%
-    st_par(., st_transform, n_cores = ncor, y = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+    st_par(., st_transform, n_cores = ncor, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
 }
 
 extract_one <- function(filename, fpa_ll) {
@@ -24,72 +24,105 @@ vars <- c('aet', 'def', 'ffwi', 'fm100', 'pdsi', 'pr', 'tmmx', 'vpd', 'vs')
 
 for (j in stat){
   for (i in vars) {
-    
-    tifs <- list.files(file.path(climate_prefix, j),
-                       pattern = i,
-                       recursive = TRUE,
-                       full.names = TRUE) %>%
-      Filter(function(x) grepl(".tif", x), .)
-    
-    if (!file.exists(file.path(summaries_dir, paste0('fpa_', j, '_', i, '_summaries.rds')))) {
-      
-      sfInit(parallel = TRUE, cpus = parallel::detectCores())
-      sfExport(list = c("fpa_ll"))
-      
-      extractions <- sfLapply(as.list(tifs),
-                              fun = extract_one,
-                              fpa_ll = fpa_ll)
-      sfStop()
-      
-      # ensure that they all have the same length
-      stopifnot(all(lapply(extractions, nrow) == nrow(fpa_ll)))
-      
-      # convert to a data frame
-      extraction_df <- extractions %>%
-        bind_cols %>%
-        as_tibble %>%
-        mutate(index = ID) %>%
-        select(-starts_with("ID")) %>%
-        rename(ID = index) %>%
-        mutate(FPA_ID = data.frame(fpa_ll)$FPA_ID) %>%
-        dplyr::select(-starts_with('X')) %>%
-        gather(variable, value, -FPA_ID, -ID) %>%
-        filter(!is.na(value)) %>%
-        mutate(FPA_ID = as.character(FPA_ID),
-               ID = as.integer(ID))
-      
-      fpa_summaries <- extraction_df %>%
-        separate(variable,
-                 into = c("variable", 'year', "varmonth"),
-                 sep = "_") %>%
-        separate(varmonth,
-                 into = c("statistic", "month"),
-                 sep = "\\.") %>%
-        mutate(year = parse_number(year),
-               month = parse_number(month)) %>%
-        arrange(ID, FPA_ID, year, month, variable)
-      
-      summary_name <- file.path(summaries_dir, paste0('fpa_', j, '_', i, '_summaries.rds'))
-      write_rds(fpa_summaries, summary_name)
-      
-      # push to S3
-      system(paste0('aws s3 sync data/summary s3://earthlab-modeling-human-ignitions/summary'))
-      
-      # to conserve space delete the large files
-      unlink(summary_name)
+
+    check_tifs <- function(j, i, ...) {
+      tif <- tryCatch(list.files(file.path(climate_prefix, j),
+                                 pattern = i,
+                                 recursive = TRUE,
+                                 full.names = TRUE) %>%
+                        Filter(function(x) grepl(".tif", x), .),
+                      error = function(c) {
+                        c$message <- paste0(c$message, " (in the", i, 'variable and ', j, 'statistic)')
+                        warning(c)
+                      }
+      )
+      if (length(tif) > 1) {
+        tif <- list.files(file.path(climate_prefix, j),
+                          pattern = i,
+                          recursive = TRUE,
+                          full.names = TRUE) %>%
+          Filter(function(x) grepl(".tif", x), .)
+      } else {
+        tif <- NULL
+      }
     }
-    
-    # save extraction and summary file for each variable
-    extract_name <- file.path(summaries_dir, paste0('fpa_', j, '_', i, '_extractions.rds'))
-    if (!file.exists(extract_name)) {
-      write_rds(extractions, extract_name)
-      
-      # push all created csv climate variables
-      system(paste0('aws s3 sync data/summary s3://earthlab-modeling-human-ignitions/summary'))
-      
-      # to conserve space delete the large files
-      unlink(extract_name)
+
+    tifs <- check_tifs(j, i)
+
+    if (!is.null(tifs)) {
+
+      tifs <- list.files(file.path(climate_prefix, j),
+                         pattern = i,
+                         recursive = TRUE,
+                         full.names = TRUE) %>%
+        Filter(function(x) grepl(".tif", x), .)
+
+      if (!file.exists(file.path(summaries_dir, paste0('fpa_', j, '_', i, '_summaries.rds')))) {
+
+        sfInit(parallel = TRUE, cpus = parallel::detectCores())
+        sfExport(list = c("fpa_ll"))
+
+        extractions <- sfLapply(as.list(tifs),
+                                fun = extract_one,
+                                fpa_ll = fpa_ll)
+        sfStop()
+
+        # ensure that they all have the same length
+        stopifnot(all(lapply(extractions, nrow) == nrow(fpa_ll)))
+
+        # convert to a data frame
+        print(paste0("Working on extraction cleaning for ", i, ' in the ', j, ' statistic'))
+        extraction_df <- extractions %>%
+          bind_cols %>%
+          as_tibble %>%
+          mutate(index = ID) %>%
+          select(-starts_with("ID")) %>%
+          rename(ID = index) %>%
+          mutate(FPA_ID = data.frame(fpa_ll)$FPA_ID) %>%
+          dplyr::select(-starts_with('X')) %>%
+          gather(variable, value, -FPA_ID, -ID) %>%
+          filter(!is.na(value)) %>%
+          mutate(FPA_ID = as.character(FPA_ID),
+                 ID = as.integer(ID))
+
+        print(paste0("Working on final summaries for ", i, ' in the ', j, ' statistic'))
+        fpa_summaries <- extraction_df %>%
+          separate(variable,
+                   into = c("variable", 'year', "varmonth"),
+                   sep = "_") %>%
+          separate(varmonth,
+                   into = c("statistic", "month"),
+                   sep = "\\.") %>%
+          mutate(year = parse_number(year),
+                 month = parse_number(month)) %>%
+          arrange(ID, FPA_ID, year, month, variable)
+
+        summary_name <- file.path(summaries_dir, paste0('fpa_', j, '_', i, '_summaries.rds'))
+        write_rds(fpa_summaries, summary_name)
+
+        # push to S3
+        system(paste0('aws s3 cp data/summary s3://earthlab-modeling-human-ignitions/summary --recursive'))
+
+        # to conserve space delete the large files
+        unlink(summary_name)
+      }
+
+      # save extraction and summary file for each variable
+      extract_name <- file.path(summaries_dir, paste0('fpa_', j, '_', i, '_extractions.rds'))
+      if (!file.exists(extract_name)) {
+        write_rds(extractions, extract_name)
+
+        # push all created csv climate variables
+        system(paste0('aws s3 cp data/summary s3://earthlab-modeling-human-ignitions/summary --recursive'))
+
+        # to conserve space delete the large files
+        unlink(extract_name)
+      }
+      gc()
+    } else {
+
+      print(paste0("Skipping ", i, ' in the ', j, ' statistic'))
+
     }
-    gc()
   }
 }
