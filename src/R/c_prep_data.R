1ad7cfe5c91afba3454954ac1cb2fb52dd97159e
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-03 14:53:18 -0700

    added terrain analysis

--- src/R/c_prep_data.R
+++ src/R/c_prep_data.R
@@ -18,9 +18,9 @@ if (!exists("ecoregions")){
     sf::st_simplify(., preserveTopology = TRUE, dTolerance = 1000)  %>%
     sf::st_transform(st_crs(usa_shp)) %>%
     dplyr::mutate(NA_L3NAME = as.character(NA_L3NAME),
-           NA_L3NAME = ifelse(NA_L3NAME == 'Chihuahuan Desert',
-                              'Chihuahuan Deserts',
-                              NA_L3NAME))
+                  NA_L3NAME = ifelse(NA_L3NAME == 'Chihuahuan Desert',
+                                     'Chihuahuan Deserts',
+                                     NA_L3NAME))
 }
 
 # Create raster mask
@@ -32,8 +32,8 @@ if (!exists("fishnet_4k")) {
       sf::st_intersection(., st_union(usa_shp))
 
     sf::st_write(fishnet_4k,
-             file.path(fishnet_path, "fishnet_4k.gpkg"),
-             driver = "GPKG")
+                 file.path(fishnet_path, "fishnet_4k.gpkg"),
+                 driver = "GPKG")
 
     system(paste0("aws s3 cp ",
                   fishnet_path, "/fishnet_4k.gpkg ",
@@ -46,7 +46,7 @@ if (!exists("fpa_clean")) {
   if (!file.exists(file.path(processed_dir, "fpa_clean.gpkg"))){
     if (!exists("fpa")) {
       fpa <- sf::st_read(dsn = file.path(fpa_prefix, "Data", "FPA_FOD_20170508.gdb"),
-                     layer = "Fires", quiet= FALSE) %>%
+                         layer = "Fires", quiet= FALSE) %>%
         sf::st_transform(st_crs(usa_shp)) %>%
         sf::st_intersection(., st_union(usa_shp))
     }
@@ -55,21 +55,21 @@ if (!exists("fpa_clean")) {
       dplyr::select(FPA_ID, LATITUDE, LONGITUDE, ICS_209_INCIDENT_NUMBER, ICS_209_NAME, MTBS_ID, MTBS_FIRE_NAME,
                     FIRE_YEAR, DISCOVERY_DATE, DISCOVERY_DOY, STAT_CAUSE_DESCR, FIRE_SIZE, STATE)  %>%
       dplyr::mutate(cause = ifelse(STAT_CAUSE_DESCR == "Lightning", "Lightning", "Human"),
-             FIRE_SIZE_km2 = (FIRE_SIZE*4046.86)/1000000,
-             doy = day(DISCOVERY_DATE),
-             month = month(DISCOVERY_DATE),
-             year = FIRE_YEAR,
-             ym = as.yearmon(paste(year, sprintf("%02d", month),
-                                   sep = "-"))) %>%
+                    FIRE_SIZE_km2 = (FIRE_SIZE*4046.86)/1000000,
+                    doy = day(DISCOVERY_DATE),
+                    month = month(DISCOVERY_DATE),
+                    year = FIRE_YEAR,
+                    ym = as.yearmon(paste(year, sprintf("%02d", month),
+                                          sep = "-"))) %>%
       dplyr::rename_all(tolower)
 
     sf::st_write(fpa_clean,
-             file.path(processed_dir, "fpa_clean.gpkg"),
-             driver = "GPKG")
+                 file.path(processed_dir, "fpa_clean.gpkg"),
+                 driver = "GPKG")
 
-    system(paste0("aws s3 cp ",
-                  processed_dir, "/fpa_clean.gpkg",
-                  s3_proc_prefix, "fpa_clean.gpkg"))
+    system(paste0("aws s3 sync ",
+                  processed_dir, " ",
+                  s3_proc_prefix))
   }
 }
 
@@ -79,7 +79,7 @@ if (!exists("primary_rds")) {
   if (!file.exists(file.path(processed_dir, "primary_rds.gpkg"))) {
     if (!exists("rds")) {
       rds <- sf::st_read(dsn = file.path(roads_prefix, "tlgdb_2015_a_us_roads.gdb"), layer = 'Roads')
-      }
+    }
 
     primary_rds <- rds %>%
       dplyr::filter(MTFCC == "S1100") %>%
@@ -88,8 +88,8 @@ if (!exists("primary_rds")) {
       dplyr::mutate(bool_prds = 1)
 
     sf::st_write(primary_rds,
-             file.path(processed_dir, "primary_rds.gpkg"),
-             driver = "GPKG")
+                 file.path(processed_dir, "primary_rds.gpkg"),
+                 driver = "GPKG")
   } else {
 
     primary_rds <- sf::st_read(dsn = file.path(processed_dir, "primary_rds.gpkg"))
@@ -101,18 +101,18 @@ if (!exists("secondary_rds")) {
   if (!file.exists(file.path(processed_dir, "secondary_rds.gpkg"))) {
     if (!exists("rds")) {
       rds <- sf::st_read(dsn = file.path(roads_prefix, "tlgdb_2015_a_us_roads.gdb"), layer = 'Roads')
-      }
+    }
 
-        secondary_rds <- rds %>%
-          dplyr::filter(MTFCC == "S1200") %>%
-          sf::st_transform(p4string_ea) %>%
-          sf::st_intersection(., usa_shp) %>%
-          dplyr::mutate(bool_srds = 1)
+    secondary_rds <- rds %>%
+      dplyr::filter(MTFCC == "S1200") %>%
+      sf::st_transform(p4string_ea) %>%
+      sf::st_intersection(., usa_shp) %>%
+      dplyr::mutate(bool_srds = 1)
 
 
-        sf::st_write(secondary_rds,
-             file.path(processed_dir, "secondary_rds.gpkg"),
-             driver = "GPKG")
+    sf::st_write(secondary_rds,
+                 file.path(processed_dir, "secondary_rds.gpkg"),
+                 driver = "GPKG")
   } else {
 
     secondary_rds <- sf::st_read(dsn = file.path(processed_dir, "secondary_rds.gpkg"))
@@ -124,30 +124,30 @@ if (!exists("tertiary_rds")) {
   if (!file.exists(file.path(processed_dir, "tertiary_rds.gpkg"))) {
     if (!exists("rds")) {
       rds <- sf::st_read(dsn = file.path(roads_prefix, "tlgdb_2015_a_us_roads.gdb"), layer = 'Roads')
-      }
-
-      tertiary_rds <- rds %>%
-        dplyr::filter(MTFCC == "S1400") %>%
-        sf::st_transform(p4string_ea) %>%
-        sf::st_intersection(., usa_shp) %>%
-        dplyr::mutate(bool_trds = 1)
-
-      sf::st_write(secondary_rds,
-        file.path(processed_dir, "tertiary_rds.gpkg"),
-        driver = "GPKG")
-
-      } else {
-
-        tertiary_rds <- sf::st_read(dsn = file.path(processed_dir, "tertiary_rds.gpkg"))
-      }
     }
+
+    tertiary_rds <- rds %>%
+      dplyr::filter(MTFCC == "S1400") %>%
+      sf::st_transform(p4string_ea) %>%
+      sf::st_intersection(., usa_shp) %>%
+      dplyr::mutate(bool_trds = 1)
+
+    sf::st_write(secondary_rds,
+                 file.path(processed_dir, "tertiary_rds.gpkg"),
+                 driver = "GPKG")
+
+  } else {
+
+    tertiary_rds <- sf::st_read(dsn = file.path(processed_dir, "tertiary_rds.gpkg"))
+  }
+}
 
 # All major roads
 if (!exists("all_rds")) {
   if (!file.exists(file.path(processed_dir, "all_rds.gpkg"))) {
     if (!exists("rds")) {
       rds <- sf::st_read(dsn = file.path(roads_prefix, "tlgdb_2015_a_us_roads.gdb"), layer = 'Roads')
-      }
+    }
 
     all_rds <- rds %>%
       dplyr::filter(MTFCC == "S1200" | MTFCC == "S1200") %>%
@@ -156,8 +156,8 @@ if (!exists("all_rds")) {
       dplyr::mutate(bool_ards = 1)
 
     sf::st_write(all_rds,
-             file.path(processed_dir, "all_rds.gpkg"),
-             driver = "GPKG")
+                 file.path(processed_dir, "all_rds.gpkg"),
+                 driver = "GPKG")
   }
 }
 
@@ -170,10 +170,10 @@ if (!exists("rail_rds")) {
       dplyr::mutate(bool_rrds = 1)
 
     sf::st_write(rail_rds,
-             file.path(anthro_dir, "rail_rds.gpkg"),
-             driver = "GPKG",
-             update=TRUE,
-             delete_dsn=TRUE)
+                 file.path(anthro_dir, "rail_rds.gpkg"),
+                 driver = "GPKG",
+                 update=TRUE,
+                 delete_dsn=TRUE)
   }
 }
 
@@ -188,8 +188,8 @@ if (!exists("tl")) {
       dplyr::filter(st_is(., c("LINESTRING")))
 
     sf::st_write(tl,
-             file.path(anthro_dir, "power_lines.gpkg"),
-             driver = "GPKG")
+                 file.path(anthro_dir, "power_lines.gpkg"),
+                 driver = "GPKG")
   }
 }
 
@@ -203,19 +203,24 @@ mosaic_rasters <- function(files){
   list_rasters <- function(list_names) {
     raster_list <- list() # initialise the list of rasters
     for (i in 1:(length(list_names))){
+
       rst_name <- list_names[i] # list_names contains all the names of the images in .grd format
       raster_file <- raster::raster(rst_name)
+
     }
     raster_list <- append(raster_list, raster_file) # update raster_list at each iteration
   }
 
   #convert every raster path to a raster object and create list of the results
-  raster_list <- sapply(list_names, FUN = list_rasters)
+  raster_list <- sapply(files, FUN = list_rasters)
 
-  # edit settings of the raster list for use in do.call and mosaic
+  # make all raster names null
   names(raster_list) <- NULL
 
-  #run do call to implement mosaic over the list of raster objects.
+  # take the mean of overlapping raster images
+  raster_list$fun <- mean
+
+  # mosaic all rasters in list
   mos <- do.call(raster::mosaic, raster_list)
 
   #set crs of output
@@ -226,7 +231,7 @@ mosaic_rasters <- function(files){
 elev_files <- list.files(file.path(raw_prefix, 'gtopo30'),
                          pattern = '.tif',
                          recursive = TRUE,
-                         full.names = TRUE))
+                         full.names = TRUE)
 
 if (!exists("elevation")) {
   if (!file.exists(file.path(processed_dir, 'elevation.tif'))) {
@@ -239,13 +244,13 @@ if (!exists("elevation")) {
     raster::writeRaster(elevation, filename = file.path(processed_dir, "elevation.tif"), format = "GTiff")
 
     system(paste0("aws s3 sync ",
-                  file.path(processed_dir, "elevation.tif"), " ",
-                  s3_proc_prefix, "elevation.tif"))
+                  processed_dir, " ",
+                  s3_proc_prefix))
 
-    } else {
+  } else {
 
-      elevation <- raster::raster(file.path(processed_dir, "elevation.tif"))
-    }
+    elevation <- raster::raster(file.path(processed_dir, "elevation.tif"))
+  }
 }
 
 # Create slope raster
@@ -258,13 +263,13 @@ if (!exists("slope")) {
     raster::writeRaster(slope, filename = file.path(processed_dir, "slope.tif"), format = "GTiff")
 
     system(paste0("aws s3 sync ",
-                  file.path(processed_dir, "slope.tif"), " ",
-                  s3_proc_prefix, "slope.tif"))
+                  processed_dir, " ",
+                  s3_proc_prefix))
 
-    } else {
+  } else {
 
-      slope <- raster::raster(file.path(processed_dir, "slope.tif"))
-    }
+    slope <- raster::raster(file.path(processed_dir, "slope.tif"))
+  }
 }
 
 # Create terrain roughness
@@ -277,8 +282,8 @@ if (!exists("roughness")) {
     raster::writeRaster(roughness, filename = file.path(processed_dir, "roughness.tif"), format = "GTiff")
 
     system(paste0("aws s3 sync ",
-                  file.path(processed_dir, "roughness.tif"), " ",
-                  s3_proc_prefix, "roughness.tif"))
+                  processed_dir, " ",
+                  s3_proc_prefix))
 
   } else {
 
@@ -296,8 +301,8 @@ if (!exists("aspect")) {
     raster::writeRaster(aspect, filename = file.path(processed_dir, "aspect.tif"), format = "GTiff")
 
     system(paste0("aws s3 sync ",
-                  file.path(processed_dir, "aspect.tif"), " ",
-                  s3_proc_prefix, "aspect.tif"))
+                  processed_dir, " ",
+                  s3_proc_prefix))
 
   } else {
 
