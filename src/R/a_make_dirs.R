fd99a9892fbdd8f547308371542d7043614de85b
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-05 15:48:58 -0700

    commented and added libraries

--- src/R/a_make_dirs.R
+++ src/R/a_make_dirs.R
@@ -1,22 +1,30 @@
 
-x <- c("raster", "ncdf4", "tidyverse", "sf", "rasterVis", "gridExtra", "data.table", "assertthat", "rvest", 'parallel',
-       "httr", "purrr", "rgdal", "maptools", "foreign", "purrr", "zoo", "lubridate", "magrittr", "snowfall")
+# load all ibraries
+x <- c("raster", "ncdf4", "tidyverse", "sf", "rasterVis", "gridExtra", "data.table", "assertthat", "rvest", 'parallel', 'doParallel',
+       'parallel', 'foreach', "httr", "purrr", "rgdal", "maptools", "foreign", "purrr", "zoo", "lubridate", "magrittr", "snowfall")
 lapply(x, library, character.only = TRUE, verbose = FALSE)
 
+# load all functions
 source('src/functions/helper_functions.R')
 source("src/functions/download-data.R")
 source('src/functions/st_par.R')
 source("src/functions/decompress_function.R")
 
-# Projections
+# key rojections
 p4string_ed <- "+proj=eqdc +lat_0=0 +lon_0=0 +lat_1=33 +lat_2=45 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"   #http://spatialreference.org/ref/esri/102005/
 p4string_ea <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"   #http://spatialreference.org/ref/sr-org/6903/
 
+# define the amount of cores st_par runs on
 ncore <- parallel::detectCores()
 
+# create main directories
 prefix <- ("data")
-raw_prefix <- file.path(prefix, "raw")
+ancillary_dir <- file.path(prefix, "ancillary")
+summary_dir <- file.path(prefix, "summary")
+processed_dir <- file.path(prefix, 'processed')
 
+# create main raw folder and all subfolders to hold raw/unprocessed data
+raw_prefix <- file.path(prefix, "raw")
 us_prefix <- file.path(raw_prefix, "cb_2016_us_state_20m")
 ecoregion_prefix <- file.path(raw_prefix, "us_eco_l3")
 fpa_prefix <- file.path(raw_prefix, "fpa-fod")
@@ -29,16 +37,14 @@ elev_prefix <- file.path(raw_prefix, 'metdata_elevationdata')
 tl_prefix <- file.path(raw_prefix, 'Electric_Power_Transmission_Lines')
 climate_prefix <- file.path(prefix, "climate")
 
-ancillary_dir <- file.path(prefix, "ancillary")
-summary_dir <- file.path(prefix, "summary")
+# create direcotires to hold climate summary outputs
 summaries_dir <- file.path(summary_dir, "fpa_climate_summaries")
 extraction_dir <- file.path(summary_dir, "fpa_climate_extraction")
 
-processed_dir <- file.path(prefix, 'processed')
 anthro_dir <- file.path(prefix, "anthro")
-
 fishnet_path <- file.path(ancillary_dir, "fishnet")
 
+# for pushing and pulling to s3 using the system function
 s3_anc_prefix <- 's3://earthlab-modeling-human-ignitions/ancillary/'
 s3_proc_prefix <- 's3://earthlab-modeling-human-ignitions/processed/'
 s3_raw_prefix <- 's3://earthlab-modeling-human-ignitions/raw/'
@@ -48,5 +54,4 @@ var_dir <- list(prefix, raw_prefix, us_prefix, ecoregion_prefix, roads_prefix, s
                 fpa_prefix, rails_prefix, pd_prefix, iclus_prefix, climate_prefix,
                 nlcd_prefix, elev_prefix, tl_prefix, ancillary_dir, anthro_dir,
                 fishnet_path, processed_dir, summaries_dir, extraction_dir)
-
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
