f049688a59a93849c9359b46ae7bfce245723969
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-07 11:41:43 -0700

    add generic data_download function to reduce copy pasta in b_get_anthro

--- src/functions/download-data.R
+++ src/functions/download-data.R
@@ -9,6 +9,23 @@ load_data <- function(url, dir, layer, outname) {
   }
  name <- paste0(outname, "_shp")
  name <- sf::st_read(dsn = dir, layer = layer)
- 
+
  name
 }
+
+
+download_data <-  function(url, dir, layer, fld_name) {
+  file <- paste0(dir, "/", layer)
+
+  if (!file.exists(file)) {
+    dest <- paste0(dir, ".zip")
+    download.file(url, destfile = paste0(dir, ".zip"))
+    unzip(dest,
+          exdir = dir)
+    unlink(dest)
+    assert_that(file.exists(file))
+
+    system(paste0('aws s3 sync ',
+                  file, " ",
+                  s3_raw_prefix, fld_name))
+  }
