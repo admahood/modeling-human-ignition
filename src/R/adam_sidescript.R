69a5cfdc3d5ea0d21f5dc1f26b8568d0e5849844
‘admahood’ <admahood@gmail.com>
2018-03-08 19:41:41 +0000

    post nate/adam data jam

--- src/R/adam_sidescript.R
+++ src/R/adam_sidescript.R
@@ -11,30 +11,56 @@ library(sf)
 ## functions
 
 # Function to calculate lengths of lines in given raster cell
-lengthInCell <- function(i, r, l) {
+lengthInCell <- function(f, sl) {
+  # i: iterator
+  # f: fishnet
+  # l: shapefile of lines
   # https://stat.ethz.ch/pipermail/r-sig-geo/2015-March/022475.html
   # thanks!
-  r[i] <- 1
-  rpoly <- rasterToPolygons(r, na.rm=T)
-  lc <- crop(l, rpoly)
-  if (!is.null(lc)) {
-    return(gLength(lc))
-  } else {
-    return(0)
-  }
+  require(snowfall)
+  require(raster)
+  require(sf)
+  require(tidyverse)
+  features <- 1:nrow(f[,])
+  parts <- split(features, cut(features, detectCores()))
+  
+  # parts[i] <- 1
+  # rpoly <- rasterToPolygons(parts, na.rm=T)
+  
+  lc <- st_intersection(sl, f[parts[[detectCores()]][1],])
+  
+  # if (!is.null(lc)) {
+  #   return(st_length(lc))
+  # } else {
+  #   return(0)
+  # }
 }
 
-density <- function(rast, layer){
+source("src/functions/helper_functions.R")
+
+#density <- function(rast, layer){
   # get the template raster (ras_mask)
-  rLength <- raster(rast)
+  # rLength <- raster(rast)
   
   # read in shapefile, transform to raster crs
-  sl <- st_read(layer) %>%
-    st_transform(crs = crs(rLength, asText = TRUE)) %>%
-    as("Spatial")
+ri <- usa_shp %>%
+  filter(STUSPS == "RI")
+
+  f <- st_read("data/ancillary/fishnet/fishnet_4k.gpkg") %>%
+    st_intersection(., ri) 
   
-  # Calculate lengths
-  lengths <- sapply(1:ncell(rLength), lengthInCell, rLength, sl)
+  sl <- st_read(layer) %>%
+      st_transform(crs = st_crs(f)) %>%
+    st_intersection(., stunion(usa_shp)) %>%
+    filter(STUSPS == "RI")
+  
+ # lengths <- sapply(1:ncell(rLength), lengthInCell, rLength, sl)
+  lengths <- sfLapply(1:detectCores(),
+                      fun = lengthInCell,
+                      f = fs,
+                      sl = sl
+                      )
+  sfStop()
   
   # cl <- makeCluster(detectCores())
   # lengths <- parSapply(cl, 1:ncell(rLength), FUN = lengthInCell, r=rLength, l=sl )
@@ -46,7 +72,7 @@ density <- function(rast, layer){
   
   #output
   return(rLength)
-}
+#}
 
 #### do the stuff ####
 dsn <- ""
@@ -82,3 +108,19 @@ plot(rLength); plot(sl, add=T)
 # }
 # 
 # listoftifs=Sys.glob("*.tif")
+
+registerDoParallel(detectCores())
+f$length <- 0
+f$length <- length_in_poly(sl,f)
+  
+length_in_poly <- function(sl,f){
+  return(foreach(i = 1:nrow(f), .combine = rbind) %dopar% {
+  x <- st_intersection(sl, f[i,])
+  if(nrow(x) > 0){
+    f$length[i] <-  sum(st_length(x))/st_area(f[i,])
+  } else{
+    f$length[i] <-0
+    }
+  
+})}
+stopCluster()
