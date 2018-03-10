ee34f2bc721401f272b068bcc6a84417e4a409f0
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-10 13:33:47 +0000

    added an additional loop to iterate through grouping variable

--- src/R/4_fpa_climate_summaries.R
+++ src/R/4_fpa_climate_summaries.R
@@ -33,17 +33,16 @@ if (!exists("fpa_ll")) {
 
 stat <- c('mean', 'days-above-95th', '95th-percentile')
 vars <- c('aet', 'def', 'ffwi', 'fm100', 'pdsi', 'pr', 'tmmx', 'vpd', 'vs')
-fpa <- fpa_ll %>%
-  slice(1:26) 
-unique_states <- unique(fpa$STATE)
+
+unique_states <- unique(fpa_ll$STATE)
 
 for (j in stat){
   for (i in vars) {
     fpa_summaries <- list()
-    
-    for (k in 1:length(unique_states)) {
+    counter <- 1
+    for (k in 1:length(unique_states)) { 
       
-      sub_df <- subset(fpa, fpa$STATE == unique_states[k])
+      sub_df <- subset(fpa_ll, fpa_ll$STATE == unique_states[k])
 
       tifs <- check_tifs(j, i)
   
@@ -53,22 +52,23 @@ for (j in stat){
   
       } else if (!file.exists(file.path(summaries_dir, paste0('fpa_', i, '_', j, '_summaries.rds')))) {
   
-        print(paste0('Working on ', j, ' ', i))
+        print(paste0('Working on ', counter, " of ", length(unique_states), " for the ", j, ' ', i))
         
-        #sfInit(parallel = TRUE, cpus = parallel::detectCores())
-        #sfExport(list = c("sub_df"))
-  
-        extractions <- lapply(as.list(tifs),
-                                FUN = extract_one,
-                                shapefile_extractor = as(sub_df, 'Spatial'))}
-        #sfStop()
-  
+        cl <- makeCluster(detectCores())
+        registerDoParallel(cl)  
+        
+        extractions <- foreach (i = tifs) %dopar% {
+          
+          res <- raster::extract(raster::stack(i), sub_df,
+                                 na.rm = TRUE, fun = 'mean', df = TRUE)
+        }
+        stopCluster(cl)
+
+
         # ensure that they all have the same length
-        stopifnot(all(lapply(extractions, nrow) == nrow(sub_df)))}
+        stopifnot(all(lapply(extractions, nrow) == nrow(sub_df)))
   
         # convert to a data frame
-        print(paste0("Creating extraction dataframe for ", i, ' ', j, ' summaries'))
-  
         extraction_df <- extractions %>%
           bind_cols %>%
           as_tibble %>%
@@ -88,25 +88,26 @@ for (j in stat){
           mutate(day = '01',
                  year_month_day = as.Date(paste(year, month, day, sep='-')))
         
-        print(paste0("Creating fpa summaries for ", cause, i, ' ', j))
-        
         sub_df <- sub_df %>%
           dplyr::select(FPA_ID, year_month_day)
         
         fpa_summaries[[k]] <- get_climate_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 24) %>%
           dplyr::select(-year_month_day, - ymd_lagged)
-
-        # creating the final dataframe takes about 150 GB of RAM, this clears the cache so it can be run again
-        gc()
+        
+        counter <- counter + 1
       }
     }
     fpa_summaries <- do.call(rbind, fpa_summaries) # Convert to data frame format
     
     # save the final cleaned climate extractions joined with the fpa-fod database
     summary_name <- file.path(summaries_dir, paste0('fpa_', i, '_', j, '_summaries.rds'))
-    write_rds(fpa_out, summary_name)
+    write_rds(fpa_summaries, summary_name)
     
     # push to S3
-    #system('aws s3 sync modeling-human-ignition/data/summary s3://earthlab-modeling-human-ignitions/summary')
+    system('aws s3 sync modeling-human-ignition/data/summary s3://earthlab-modeling-human-ignitions/summary')
+    
+    # creating the final dataframe takes about 150 GB of RAM, this clears the cache so it can be run again
+    gc()
   }
 }
+
