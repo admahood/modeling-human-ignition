9e064630ea4ad96b93dd7a6fb99c09a983fed19e
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-15 16:43:58 -0600

    added parallelizing loop - prototype

--- src/R/c_data_prep_human_density.R
+++ src/R/c_data_prep_human_density.R
@@ -9,8 +9,8 @@ pop_den <- st_read(file.path(pd_prefix, 'us_pbg00_2007.gdb')) %>%
 fp_pop_den <- fpa_clean  %>%
   st_intersection(., pop_den) %>%
   as_tibble() %>%
-  dplyr::select(FPA_ID, HDEN90, HDEN00, HDEN10, HDEN20, HDEN30) %>%
-  gather(variable, value, -FPA_ID) %>%
+  dplyr::select(FPA_ID, STATE, HDEN90, HDEN00, HDEN10, HDEN20, HDEN30) %>%
+  gather(variable, value, -FPA_ID, -STATE) %>%
   mutate(value = ifelse(value == -999, is.na(value), value)) %>%
   filter(!is.na(value)) %>%
   mutate(year = case_when(
@@ -25,24 +25,28 @@ fp_pop_den <- fpa_clean  %>%
   ungroup %>%
   dplyr::select(-value)
 
+unique_states <- unique(fp_pop_den$STATE)
 
-# Then interpolate for each month and year from 1984 - 2015
-# using a simple linear sequence
-impute_density <- function(df) {
-  year_seq <- min(df$year):max(df$year)
-  predict_seq <- seq(min(df$year),
-                     max(df$year),
-                     length.out = (length(year_seq) - 1) * 12)
-  preds <- approx(x = df$year,
-                  y = df$housing_density,
-                  xout = predict_seq)
-  res <- as_tibble(preds) %>%
-    rename(t = x, housing_density = y) %>%
-    mutate(year = floor(t),
-           month = rep(1:12, times = length(year_seq) - 1)) %>%
-    filter(year < 2030)
-  res$FPA_ID <- unique(df$FPA_ID)
-  res
+fp_pop_den_summaries <- list()
+counter <- 1
+
+# subset the fpa-fod data based on state grouping variable
+# this increases the speed of this function and only needs ~40GB of memory max.
+for (k in unique_states) {
+
+  # create a subdataframe based on state subset
+  sub_df <- subset(fp_pop_den, fp_pop_den$STATE == unique_states[k])
+
+  print(paste0('Working on ', counter, " of ", length(unique_states)))
+
+  sub_id <- faster_as_tibble(sub_df, sub_df$FPA_ID)
+
+  sfInit(parallel = TRUE, cpus = parallel::detectCores())
+  #sfExport(list = c("ecoregion_shp"))
+
+  extractions <- sfLapply(sub_id,
+                          fun = impute_density)
+  sfStop()
 }
 
 res <- fp_pop_den %>%
