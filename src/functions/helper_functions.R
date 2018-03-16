363bc29c1974465286f4174247b1855905c225d6
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-16 18:52:54 +0000

    changed impute and made get_lags more general

--- src/functions/helper_functions.R
+++ src/functions/helper_functions.R
@@ -1,7 +1,7 @@
 # General Helper functions
 
 faster_as_tibble <- function(x) {
-  structure(x, class = c("tbl_df", "tbl", "data.frame", "sfc"), row.names = as.character(seq_along(x[[1]])))
+  structure(x, class = c("tbl_df", "tbl", "data.frame"), row.names = as.character(seq_along(x[[1]])))
 }
 
 split_fast_tibble <- function (x, f, drop = FALSE, ...) {
@@ -18,7 +18,7 @@ extract_one <- function(filename, shapefile_extractor) {
   # inputs:
   # filename -> a list of all tif filenames with full path
   # shapefile_extractor -> the shapefile (point or polygon) to extract climate data
-
+  
   out_name <- gsub('.tif', '.csv', filename)
   if (!file.exists(out_name)) {
     res <- raster::extract(raster::stack(filename), shapefile_extractor,
@@ -33,7 +33,7 @@ extract_one <- function(filename, shapefile_extractor) {
 check_tifs <- function(j, i, ...) {
   # checks whether the statistic being evaluated has the variable data
   # if not then returns NULL which allows the larger loop below to skip the inputs
-
+  
   tif <- tryCatch(list.files(file.path(climate_prefix, j),
                              pattern = i,
                              recursive = TRUE,
@@ -44,7 +44,7 @@ check_tifs <- function(j, i, ...) {
                     warning(c)
                   }
   )
-
+  
   # if the length of the tryCatch is greater than one then that indicates there
   # are data in the statistic/variable combination
   if (length(tif) > 1) {
@@ -56,117 +56,123 @@ check_tifs <- function(j, i, ...) {
   } else {
     # if the tif length is 0 then that indicates there are no climate data for
     # that statistic/variable combination
-
+    
     tif <- NULL
   }
 }
 
-get_climate_lags <- function(fpa_df, climate_df, start_date, time_lag) {
-
+get_lags <- function(extract_to, extract_from, start_date, time_lag) {
+  
   # capture the variable name and statistic to be incorporated in the output column name
-  variable <- paste0(climate_df$variable[1], '_', climate_df$statistic[1])
-
+  if (exists('extract_from$statistic')) {
+    variable <- paste0(extract_from$variable[1], '_', extract_from$statistic[1])
+  } else {
+    variable <- 'housing_density'
+  }
+  
   # internal function to create a lagged date
   lag_date <- function(start_date, time_lag) {
     require(magrittr)
     require(tidyverse)
     require(lubridate)
-
+    
     # breakup the start date into its components
     y <- year(start_date)
     m <- month(start_date)
     d <- day(start_date)
-
+    
     # calculate the new lagged year
     y <- y + (m - time_lag - 1) %/% 12
-
+    
     # calculate the new lagged month
     m <- ifelse(((m - time_lag) %% 12) == 0, 12, (m - time_lag) %% 12)
-
+    
     # stitch the new lagged date together
     as.Date(paste0(y, "-", m, "-", d))
   }
-
+  
   # remove the sf data - increases efficiency
-  fpa_df <- fpa_df %>%
-    as.data.frame() %>%
-    select(-geom)
-
-  # pair down to the climate_df to allow for easier left_join
-  climate_df <- climate_df %>%
+  if (exists('extract_to$geom')) {
+    extract_to <- extract_to %>%
+      as.data.frame() %>%
+      select(-geom)
+  }
+  
+  # pair down to the extract_from to allow for easier left_join
+  extract_from <- extract_from %>%
     select('FPA_ID', 'year_month_day', 'value')
-
+  
   for (j in 0:time_lag) {
     require(magrittr)
     require(tidyverse)
-
+    
     # create a lagged data year column that can be joined and extracted upon
-    fpa_df[, paste0(variable, '_lag_', j)]  <- fpa_df %>%
+    extract_to[, paste0(variable, '_lag_', j)]  <- extract_to %>%
       dplyr::mutate(ymd_lagged = lag_date(start_date = start_date, time_lag = j)) %>%
-      left_join(., climate_df, by = c('FPA_ID', 'ymd_lagged' = 'year_month_day')) %>%
+      left_join(., extract_from, by = c('FPA_ID', 'ymd_lagged' = 'year_month_day')) %>%
       dplyr::select(value)
   }
-  return(fpa_df)
+  return(extract_to)
 }
 
 
 # functions for c_prep_data.R ---------------------------------------------
 
 mosaic_rasters <- function(files){
-
+  
   # this function will take a list of raster data with full path names and:
   #  1. read in all rasters iteratively
   #  2. create a raster list of all created rasters
   #  3. mosaic all rasters, using mean if tiles overlap.
   #
   # the only input needed is the list of raster filenames
-
+  
   #Internal function to make a list of raster objects from list of files.
   list_rasters <- function(list_names) {
-
+    
     raster_list <- list() # initialise the list of rasters
-
+    
     for (i in 1:(length(list_names))){
-
+      
       rst_name <- list_names[i] # list_names contains all the names of the images in .grd format
       raster_file <- raster::raster(rst_name)
-
+      
     }
     raster_list <- append(raster_list, raster_file) # update raster_list at each iteration
   }
-
+  
   #convert every raster path to a raster object and create list of the results
   raster_list <- sapply(files, FUN = list_rasters)
-
+  
   # make all raster names null
   names(raster_list) <- NULL
-
+  
   # take the mean of overlapping raster images
   raster_list$fun <- mean
-
+  
   # mosaic all rasters in list
   mos <- do.call(raster::mosaic, raster_list)
-
+  
   #set crs of output
   crs(mos) <- crs(x = raster(files[1]))
   return(mos)
 }
 
 length_in_poly <- function(spatial_lines, fishnet){
-
+  
   registerDoParallel(detectCores())
-
+  
   fishnet$density <- 0
-
+  
   return( foreach (i = 1:nrow(fishnet), .combine = rbind) %dopar% {
-
+    
     x <- st_intersection(spatial_lines, fishnet[i,])
-
+    
     if(nrow(x) > 0){
       fishnet$density <-  sum(st_length(x))/st_area(fishnet[i,])
-
+      
     } else{
-
+      
       fishnet$density[i] <-0
     }
   }
@@ -182,13 +188,13 @@ impute_density <- function(df) {
                      max(df$year),
                      length.out = (length(year_seq) - 1) * 12)
   preds <- approx(x = df$year,
-                  y = df$housing_density,
+                  y = df$value,
                   xout = predict_seq)
   res <- as_tibble(preds) %>%
-    rename(t = x, housing_density = y) %>%
+    rename(t = x, value = y) %>%
     mutate(year = floor(t),
            month = rep(1:12, times = length(year_seq) - 1)) %>%
-    filter(year < 2030)
+    filter(year < 2016)
   res$FPA_ID <- unique(df$FPA_ID)
   res
 }
@@ -208,7 +214,7 @@ shp_rst <- function(y, x, lvl, j){
   require(raster)
   require(sf)
   require(tidyverse)
-
+  
   outrst <- rasterize(as(y[parts[[x]],], "Spatial"), j, lvl) %>%
     projectRaster(j)
 }
@@ -216,7 +222,7 @@ shp_rst <- function(y, x, lvl, j){
 # A function to recombine the split data from `shp_rst`, calculate the distance and reproject to equal area
 combine_rst <- function(y){
   # y = shp_rst output split rasters
-
+  
   do.call(merge, y) %>%
     distance(.)  %>%
     aggregate(fact = 20, fun = mean) %>%
