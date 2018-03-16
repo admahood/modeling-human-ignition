a6c2a0eb9de8e115d192ea80babc2f5362c40c9e
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-16 18:52:38 +0000

    solved it! impute housing density in parallel and extract the HD value for the month of the wildfire event

--- src/R/c_data_prep_human_density.R
+++ src/R/c_data_prep_human_density.R
@@ -1,7 +1,7 @@
 
 # import and transform the SILVIS lab partial census block housing density data
 pop_den <- st_read(file.path(pd_prefix, 'us_pbg00_2007.gdb')) %>%
-  select(HDEN90:HDEN30) %>%
+  select(HDEN90:HDEN20) %>%
   st_transform(st_crs(usa_shp))
 
 # spatially intersect the fpa_clean sf object with the population density polygons
@@ -9,50 +9,60 @@ pop_den <- st_read(file.path(pd_prefix, 'us_pbg00_2007.gdb')) %>%
 fp_pop_den <- fpa_clean  %>%
   st_intersection(., pop_den) %>%
   as_tibble() %>%
-  dplyr::select(FPA_ID, STATE, HDEN90, HDEN00, HDEN10, HDEN20, HDEN30) %>%
-  gather(variable, value, -FPA_ID, -STATE) %>%
+  dplyr::select(FPA_ID, STATE, year_month_day, HDEN90, HDEN00, HDEN10, HDEN20) %>%
+  gather(variable, value, -FPA_ID, -STATE, -year_month_day) %>%
   mutate(value = ifelse(value == -999, is.na(value), value)) %>%
   filter(!is.na(value)) %>%
   mutate(year = case_when(
     .$variable == 'HDEN90' ~ 1990,
     .$variable == 'HDEN00' ~ 2000,
     .$variable == 'HDEN10' ~ 2010,
-    .$variable == 'HDEN20' ~ 2020,
-    .$variable == 'HDEN30' ~ 2030
+    .$variable == 'HDEN20' ~ 2020
   )) %>%
   group_by(FPA_ID, year) %>%
-  mutate(housing_density = value) %>%
-  ungroup %>%
-  dplyr::select(-value)
+  ungroup 
 
 unique_states <- unique(fp_pop_den$STATE)
 
-fp_pop_den_summaries <- list()
-counter <- 1
-
 # subset the fpa-fod data based on state grouping variable
 # this increases the speed of this function and only needs ~40GB of memory max.
-for (k in unique_states) {
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
 
+fp_pop_den_summaries <- foreach (k = unique_states, .combine = rbind) %dopar% {
+  require(tidyverse)
+  require(lubridate)
+  
   # create a subdataframe based on state subset
-  sub_df <- subset(fp_pop_den, fp_pop_den$STATE == unique_states[k])
-
+  sub_df <- subset(fp_pop_den, fp_pop_den$STATE == k)
+  
   print(paste0('Working on ', counter, " of ", length(unique_states)))
-
-  sub_id <- faster_as_tibble(sub_df, sub_df$FPA_ID)
-
-  sfInit(parallel = TRUE, cpus = parallel::detectCores())
-  #sfExport(list = c("ecoregion_shp"))
-
-  extractions <- sfLapply(sub_id,
-                          fun = impute_density)
-  sfStop()
+  
+  extraction_df <- sub_df %>%
+    split(.$FPA_ID) %>%
+    map(~impute_density(.)) %>%
+    bind_rows %>%
+    dplyr::select(-t) %>%
+    mutate(day = '01',
+           year_month_day = as.Date(paste(year, month, day, sep='-')))
+  
+  # reduce the size of the dataframe to be joined during the get_climate_lags
+  sub_df <- sub_df %>%
+    dplyr::select(FPA_ID, year_month_day) %>%
+    distinct()
+  
+  # run get_climate_lags for the prior 24 months given a fpa-fod fire event
+  # this will iteratively populate a list given each state grouping variable
+  fp_pop_den_summaries <- get_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 0) %>%
+    dplyr::select(-year_month_day) 
+  
+  return(fp_pop_den_summaries)
 }
+stopCluster(cl)
 
-res <- fp_pop_den %>%
-  split(.$FPA_ID) %>%
-  map(~impute_density(.)) %>%
-  bind_rows
+fp_pop_den_summaries %>%
+  write_rds(file.path(anthro_proc_dir, 'housing_density.rds'))
 
-res %>%
-  write_rds('data/processed/housing_density.rds')
+system(paste0("aws s3 sync ",
+              summary_dir, " ",
+              s3_proc_extractions))
