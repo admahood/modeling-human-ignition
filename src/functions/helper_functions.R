cc87ef02127865795728d107b24c9abd8590bc66
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-17 10:22:55 -0600

    added get_length function

--- src/functions/helper_functions.R
+++ src/functions/helper_functions.R
@@ -18,7 +18,7 @@ extract_one <- function(filename, shapefile_extractor) {
   # inputs:
   # filename -> a list of all tif filenames with full path
   # shapefile_extractor -> the shapefile (point or polygon) to extract climate data
-  
+
   out_name <- gsub('.tif', '.csv', filename)
   if (!file.exists(out_name)) {
     res <- raster::extract(raster::stack(filename), shapefile_extractor,
@@ -33,7 +33,7 @@ extract_one <- function(filename, shapefile_extractor) {
 check_tifs <- function(j, i, ...) {
   # checks whether the statistic being evaluated has the variable data
   # if not then returns NULL which allows the larger loop below to skip the inputs
-  
+
   tif <- tryCatch(list.files(file.path(climate_prefix, j),
                              pattern = i,
                              recursive = TRUE,
@@ -44,7 +44,7 @@ check_tifs <- function(j, i, ...) {
                     warning(c)
                   }
   )
-  
+
   # if the length of the tryCatch is greater than one then that indicates there
   # are data in the statistic/variable combination
   if (length(tif) > 1) {
@@ -56,56 +56,56 @@ check_tifs <- function(j, i, ...) {
   } else {
     # if the tif length is 0 then that indicates there are no climate data for
     # that statistic/variable combination
-    
+
     tif <- NULL
   }
 }
 
 get_lags <- function(extract_to, extract_from, start_date, time_lag) {
-  
+
   # capture the variable name and statistic to be incorporated in the output column name
   if (exists('extract_from$statistic')) {
     variable <- paste0(extract_from$variable[1], '_', extract_from$statistic[1])
   } else {
     variable <- 'housing_density'
   }
-  
+
   # internal function to create a lagged date
   lag_date <- function(start_date, time_lag) {
     require(magrittr)
     require(tidyverse)
     require(lubridate)
-    
+
     # breakup the start date into its components
     y <- year(start_date)
     m <- month(start_date)
     d <- day(start_date)
-    
+
     # calculate the new lagged year
     y <- y + (m - time_lag - 1) %/% 12
-    
+
     # calculate the new lagged month
     m <- ifelse(((m - time_lag) %% 12) == 0, 12, (m - time_lag) %% 12)
-    
+
     # stitch the new lagged date together
     as.Date(paste0(y, "-", m, "-", d))
   }
-  
+
   # remove the sf data - increases efficiency
   if (exists('extract_to$geom')) {
     extract_to <- extract_to %>%
       as.data.frame() %>%
       select(-geom)
   }
-  
+
   # pair down to the extract_from to allow for easier left_join
   extract_from <- extract_from %>%
     select('FPA_ID', 'year_month_day', 'value')
-  
+
   for (j in 0:time_lag) {
     require(magrittr)
     require(tidyverse)
-    
+
     # create a lagged data year column that can be joined and extracted upon
     extract_to[, paste0(variable, '_lag_', j)]  <- extract_to %>%
       dplyr::mutate(ymd_lagged = lag_date(start_date = start_date, time_lag = j)) %>%
@@ -119,65 +119,87 @@ get_lags <- function(extract_to, extract_from, start_date, time_lag) {
 # functions for c_prep_data.R ---------------------------------------------
 
 mosaic_rasters <- function(files){
-  
+
   # this function will take a list of raster data with full path names and:
   #  1. read in all rasters iteratively
   #  2. create a raster list of all created rasters
   #  3. mosaic all rasters, using mean if tiles overlap.
   #
   # the only input needed is the list of raster filenames
-  
+
   #Internal function to make a list of raster objects from list of files.
   list_rasters <- function(list_names) {
-    
+
     raster_list <- list() # initialise the list of rasters
-    
+
     for (i in 1:(length(list_names))){
-      
+
       rst_name <- list_names[i] # list_names contains all the names of the images in .grd format
       raster_file <- raster::raster(rst_name)
-      
+
     }
     raster_list <- append(raster_list, raster_file) # update raster_list at each iteration
   }
-  
+
   #convert every raster path to a raster object and create list of the results
   raster_list <- sapply(files, FUN = list_rasters)
-  
+
   # make all raster names null
   names(raster_list) <- NULL
-  
+
   # take the mean of overlapping raster images
   raster_list$fun <- mean
-  
+
   # mosaic all rasters in list
   mos <- do.call(raster::mosaic, raster_list)
-  
+
   #set crs of output
   crs(mos) <- crs(x = raster(files[1]))
   return(mos)
 }
 
-length_in_poly <- function(spatial_lines, fishnet){
-  
-  registerDoParallel(detectCores())
-  
-  fishnet$density <- 0
-  
-  return( foreach (i = 1:nrow(fishnet), .combine = rbind) %dopar% {
-    
-    x <- st_intersection(spatial_lines, fishnet[i,])
-    
-    if(nrow(x) > 0){
-      fishnet$density <-  sum(st_length(x))/st_area(fishnet[i,])
-      
-    } else{
-      
-      fishnet$density[i] <-0
-    }
+get_density <- function(unique_groups, list_of_grids, list_of_lines) {
+
+  cl <- makeCluster(detectCores())
+  registerDoParallel(cl)
+
+  foreach (k = unique_groups, .combine = rbind) %dopar% {
+
+    require(tidyverse)
+    require(lubridate)
+    require(sf)
+
+    # create a subdataframe based on state subset
+    sub_grid <- list_of_grids[k] %>%
+      do.call(rbind, .) %>%
+      sf::st_as_sf(.)
+
+    sub_line <- list_of_lines[k] %>%
+      do.call(rbind, .) %>%
+      sf::st_as_sf(.)
+
+    single_lines <- sub_line %>%
+      dplyr::group_by(STUSPS) %>%
+      dplyr::summarise()
+
+    single_lines_hexid <- single_lines %>%
+      sf::st_cast(., "MULTILINESTRING", group_or_split = FALSE) %>%
+      sf::st_intersection(., sub_grid) %>%
+      dplyr::select(hexid4k, STUSPS, geometry) %>%
+      dplyr::mutate(length_line = st_length(.))
+
+    sub_grid <- sub_grid %>%
+      sf::st_join(., single_lines_hexid, join = st_intersects) %>%
+      dplyr::mutate(hexid4k = hexid4k.x,
+             length_line = length_line.y,
+             length_line = ifelse(is.na(length_line), 0, length_line),
+             pixel_area = as.numeric(st_area(geom)),
+             density = length_line/pixel_area,
+             STUSPS = STUSPS.x) %>%
+      dplyr::select(hexid4k, STUSPS, length_line, pixel_area, density)
+    return(sub_grid)
   }
-  )
-  stopCluster()
+  stopCluster(cl)
 }
 
 # Then interpolate for each month and year from 1984 - 2015
@@ -214,7 +236,7 @@ shp_rst <- function(y, x, lvl, j){
   require(raster)
   require(sf)
   require(tidyverse)
-  
+
   outrst <- rasterize(as(y[parts[[x]],], "Spatial"), j, lvl) %>%
     projectRaster(j)
 }
@@ -222,7 +244,7 @@ shp_rst <- function(y, x, lvl, j){
 # A function to recombine the split data from `shp_rst`, calculate the distance and reproject to equal area
 combine_rst <- function(y){
   # y = shp_rst output split rasters
-  
+
   do.call(merge, y) %>%
     distance(.)  %>%
     aggregate(fact = 20, fun = mean) %>%
