8fe736f6213e9ea02069835877a18bcc5d241433
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-18 11:30:33 -0600

    now this is broken - the extraction_df tables are too long.

--- src/R/c_data_prep_human_density.R
+++ src/R/c_data_prep_human_density.R
@@ -1,64 +1,111 @@
 
 # import and transform the SILVIS lab partial census block housing density data
-pop_den <- st_read(file.path(pd_prefix, 'us_pbg00_2007.gdb')) %>%
-  select(HDEN90:HDEN20) %>%
-  st_transform(st_crs(usa_shp))
+# clean, rename, and group the housing denisty data
+# orignially grouped by FPA id but 3k polygons is a lot easier to compute than 1.8M fire points
+if (!exists("pop_den")) {
+  if (file.exists(file.path(processed_dir, "pop_den_long.gpkg"))){
 
-# spatially intersect the fpa_clean sf object with the population density polygons
-# clean, rename, and group the housing denisty data by fpa_id
-fp_pop_den <- fpa_clean  %>%
-  st_intersection(., pop_den) %>%
-  as_tibble() %>%
-  dplyr::select(FPA_ID, STATE, year_month_day, HDEN90, HDEN00, HDEN10, HDEN20) %>%
-  gather(variable, value, -FPA_ID, -STATE, -year_month_day) %>%
-  mutate(value = ifelse(value == -999, is.na(value), value)) %>%
-  filter(!is.na(value)) %>%
-  mutate(year = case_when(
-    .$variable == 'HDEN90' ~ 1990,
-    .$variable == 'HDEN00' ~ 2000,
-    .$variable == 'HDEN10' ~ 2010,
-    .$variable == 'HDEN20' ~ 2020
-  )) %>%
-  group_by(FPA_ID, year) %>%
-  ungroup 
+    pop_den <- st_read(file.path(pd_prefix, 'us_pbg00_2007.gdb')) %>%
+      select(PBG00, HDEN90:HDEN20) %>%
+      st_transform(st_crs(usa_shp)) %>%
+      st_join(., usa_shp, join = st_intersects) %>%
+      st_intersection(fpa_clean, .) %>%
+      dplyr::select(PBG00, STUSPS, HDEN90, HDEN00, HDEN10, HDEN20) %>%
+      gather(variable, value, -PBG00, -STUSPS) %>%
+      filter(!is.na(value)) %>%
+      mutate(year = case_when(
+        .$variable == 'HDEN90' ~ 1990,
+        .$variable == 'HDEN00' ~ 2000,
+        .$variable == 'HDEN10' ~ 2010,
+        .$variable == 'HDEN20' ~ 2020
+      )) %>%
+      group_by(PBG00, year) %>%
+      ungroup
 
-unique_states <- unique(fp_pop_den$STATE)
+    sf::st_write(pop_den,
+                 file.path(processed_dir, "pop_den_long.gpkg"),
+                 driver = "GPKG")
 
-# subset the fpa-fod data based on state grouping variable
-# this increases the speed of this function and only needs ~40GB of memory max.
-cl <- makeCluster(detectCores())
-registerDoParallel(cl)
+    system(paste0("aws s3 sync ",
+                  processed_dir, " ",
+                  s3_proc_prefix))
+  }
+}
 
-fp_pop_den_summaries <- foreach (k = unique_states, .combine = rbind) %dopar% {
-  require(tidyverse)
-  require(lubridate)
-  
-  # create a subdataframe based on state subset
-  sub_df <- subset(fp_pop_den, fp_pop_den$STATE == k)
-  
-  print(paste0('Working on ', counter, " of ", length(unique_states)))
-  
-  extraction_df <- sub_df %>%
-    split(.$FPA_ID) %>%
-    map(~impute_density(.)) %>%
-    bind_rows %>%
-    dplyr::select(-t) %>%
-    mutate(day = '01',
-           year_month_day = as.Date(paste(year, month, day, sep='-')))
-  
-  # reduce the size of the dataframe to be joined during the get_climate_lags
-  sub_df <- sub_df %>%
-    dplyr::select(FPA_ID, year_month_day) %>%
-    distinct()
-  
-  # run get_climate_lags for the prior 24 months given a fpa-fod fire event
-  # this will iteratively populate a list given each state grouping variable
-  fp_pop_den_summaries <- get_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 0) %>%
-    dplyr::select(-year_month_day) 
-  
+slim <- pop_den %>%
+  filter(STATE %in% c("RI", "CT"))
+slim$STATE <- droplevels(slim$STATE)
+
+unique_states <- unique(slim$STATE)
+
+# Internal function
+# Then interpolate for each month and year from 1984 - 2015
+# using a simple linear sequence
+impute_density <- function(df) {
+  year_seq <- min(df$year):max(df$year)
+  predict_seq <- seq(min(df$year),
+                     max(df$year),
+                     length.out = (length(year_seq) - 1) * 12)
+  preds <- approx(x = df$year,
+                  y = df$value,
+                  xout = predict_seq)
+  res <- as_tibble(preds) %>%
+    rename(t = x, value = y) %>%
+    mutate(year = floor(t),
+           month = rep(1:12, times = length(year_seq) - 1)) %>%
+    filter(year < 2016)
+  res$PBG00 <- unique(df$PBG00)
+  res
+}
+
+impute_in_parallel <- function (unique_groups, input_tibble, sub_groups) {
+
+  # subset the fpa-fod data based on state grouping variable
+  # this increases the speed of this function and only needs ~40GB of memory max.
+  cl <- makeCluster(detectCores())
+  registerDoParallel(cl)
+
+  fp_pop_den_summaries <- foreach (k = unique_groups) %dopar% {
+    require(tidyverse)
+    require(lubridate)
+    k <- 'RI'
+    # create a subdataframe based on state subset
+    sub_df <- subset(input_tibble, input_tibble[[sub_groups]] == k)
+
+    extraction_df <- sub_df %>%
+      split(.$FPA_ID) %>%
+      map(~impute_density(.)) %>%
+      bind_rows %>%
+      dplyr::select(-t) %>%
+      mutate(day = '01',
+             year_month_day = as.Date(paste(year, month, day, sep='-')))
+
+    # reduce the size of the dataframe to be joined during the get_climate_lags
+    sub_df <- sub_df %>%
+      dplyr::select(FPA_ID, year_month_day) %>%
+      distinct()
+
+    # run get_climate_lags for the prior 24 months given a fpa-fod fire event
+    # this will iteratively populate a list given each state grouping variable
+    fp_pop_den_summaries <- get_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 0) %>%
+      dplyr::select(-year_month_day)
+
+    write_rds(fp_pop_den_summaries, file.path(per_state, paste0('pop_den_summary_', k, '.rds')))
+
+    system(paste0("aws s3 sync ",
+                  summary_dir, " ",
+                  s3_proc_extractions))
+
+    return(fp_pop_den_summaries)
+  }
+  stopCluster(cl)
   return(fp_pop_den_summaries)
 }
-stopCluster(cl)
+
+
+fp_pop_den_summaries <- impute_in_parallel(unique_groups= unique_states,
+                                           input_tibble = slim,
+                                           sub_groups = "STATE")
 
 fp_pop_den_summaries %>%
   write_rds(file.path(anthro_proc_dir, 'housing_density.rds'))
