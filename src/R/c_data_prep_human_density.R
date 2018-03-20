02cee0e7a6c86c06fe1ce480a36c7c5444d090a8
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-19 19:28:44 -0600

    changed dirs

--- src/R/c_data_prep_human_density.R
+++ src/R/c_data_prep_human_density.R
@@ -2,17 +2,20 @@
 # import and transform the SILVIS lab partial census block housing density data
 # clean, rename, and group the housing denisty data
 # orignially grouped by FPA id but 3k polygons is a lot easier to compute than 1.8M fire points
+
 if (!exists("pop_den")) {
   if (file.exists(file.path(processed_dir, "pop_den_long.gpkg"))){
 
     pop_den <- st_read(file.path(pd_prefix, 'us_pbg00_2007.gdb')) %>%
       select(PBG00, HDEN90:HDEN20) %>%
       st_transform(st_crs(usa_shp)) %>%
-      st_join(., usa_shp, join = st_intersects) %>%
-      st_intersection(fpa_clean, .) %>%
-      dplyr::select(PBG00, STUSPS, HDEN90, HDEN00, HDEN10, HDEN20) %>%
-      gather(variable, value, -PBG00, -STUSPS) %>%
-      filter(!is.na(value)) %>%
+      st_join(., usa_shp, join = st_intersects)
+
+    fpa_overlay <- st_intersects(fpa_clean, pop_den)
+
+    pop_den <- pop_den[unlist(fpa_overlay), ] %>%
+      dplyr::select(PBG00, STUSPS, HDEN90, HDEN00, HDEN10, HDEN20, SHAPE) %>%
+      gather(variable, value, -PBG00, -STUSPS, -SHAPE) %>%
       mutate(year = case_when(
         .$variable == 'HDEN90' ~ 1990,
         .$variable == 'HDEN00' ~ 2000,
@@ -33,12 +36,36 @@ if (!exists("pop_den")) {
 }
 
 slim <- pop_den %>%
-  filter(STATE %in% c("RI", "CT"))
-slim$STATE <- droplevels(slim$STATE)
+  filter(STUSPS %in% c("RI"))
+slim$STUSPS <- droplevels(slim$STUSPS)
 
-unique_states <- unique(slim$STATE)
+fpa_ri <- fpa_clean %>%
+  filter(STATE == 'RI')
+fpa_ri$STATE <- droplevels(fpa_ri$STATE)
 
-# Internal function
+# match each ignition to an ecoregion
+if (!file.exists(file.path(processed_dir, "fpa_popden_intersect.rds"))) {
+  st_over <- function(x, y) {
+    sapply(st_intersects(x,y), function(z) if (length(z)==0) NA_integer_ else z[1])
+  }
+  ov <- st_over(fpa_ri, slim)
+  write_rds(ov, file.path(processed_dir, "fpa_popden_intersect.rds"))
+}
+ov <- read_rds(file.path(processed_dir, "fpa_popden_intersect.rds"))
+
+fpa_ri <- fpa_ri %>%
+  mutate(PBG00 = slim$PBG00[ov],
+         STUSPS = STATE) %>%
+  filter(!is.na(PBG00)) %>%
+  dplyr::select(FPA_ID, PBG00, STUSPS, year_month_day)
+
+
+
+unique_states <- unique(slim$STUSPS)
+
+pop_list <- split_fast_tibble(slim, slim$STUSPS)
+fpa_list <- split_fast_tibble(fpa_ri, fpa_ri$STUSPS)
+
 # Then interpolate for each month and year from 1984 - 2015
 # using a simple linear sequence
 impute_density <- function(df) {
@@ -52,53 +79,71 @@ impute_density <- function(df) {
   res <- as_tibble(preds) %>%
     rename(t = x, value = y) %>%
     mutate(year = floor(t),
-           month = rep(1:12, times = length(year_seq) - 1)) %>%
+           month = rep(1:12, times = length(year_seq) - 1),
+           day = '01',
+           year_month_day = as.Date(paste(year, month, day, sep='-'))) %>%
     filter(year < 2016)
   res$PBG00 <- unique(df$PBG00)
   res
 }
 
-impute_in_parallel <- function (unique_groups, input_tibble, sub_groups) {
+impute_in_parallel <- function (unique_groups, pop_tibble, fpa_tibble, sub_groups) {
 
   # subset the fpa-fod data based on state grouping variable
   # this increases the speed of this function and only needs ~40GB of memory max.
-  cl <- makeCluster(detectCores())
-  registerDoParallel(cl)
+  #cl <- makeCluster(detectCores())
+  #registerDoParallel(cl)
 
-  fp_pop_den_summaries <- foreach (k = unique_groups) %dopar% {
+  k <- 'RI'
+  sub_groups <- 'STUSPS'
+  pop_tibble <- pop_list
+  fpa_tibble <- fpa_list
+
+  fp_pop_den_summaries <- for (k in unique_groups) {
     require(tidyverse)
     require(lubridate)
-    k <- 'RI'
+
+    pop_df <- pop_tibble[k] %>%
+      do.call(rbind, .)
+
+    fpa_df <- fpa_tibble[k] %>%
+      do.call(rbind, .)
+
+    each_id_in_pop_list <- split_fast_tibble(pop_df, pop_df$PBG00)
+    each_id_in_fpa_list <- split_fast_tibble(fpa_df, fpa_df$PBG00)
+
+    unique_popid <- unique(each_pop_id_list$PBG00)
+
+    foreach (j = unique_popid) %dopar% {
     # create a subdataframe based on state subset
-    sub_df <- subset(input_tibble, input_tibble[[sub_groups]] == k)
 
-    extraction_df <- sub_df %>%
-      split(.$FPA_ID) %>%
-      map(~impute_density(.)) %>%
-      bind_rows %>%
-      dplyr::select(-t) %>%
-      mutate(day = '01',
-             year_month_day = as.Date(paste(year, month, day, sep='-')))
+      extraction_df <- pop_df %>%
+        split(.$PBG00) %>%
+        map(~impute_density(.)) %>%
+        bind_rows %>%
+        dplyr::select(-t) %>%
+        mutate(day = '01',
+               year_month_day = as.Date(paste(year, month, day, sep='-')))
 
-    # reduce the size of the dataframe to be joined during the get_climate_lags
-    sub_df <- sub_df %>%
-      dplyr::select(FPA_ID, year_month_day) %>%
-      distinct()
+      # reduce the size of the dataframe to be joined during the get_climate_lags
+      fpa_df <- fpa_df %>%
+        dplyr::select(PBG00, year_month_day) %>%
+        distinct()
 
-    # run get_climate_lags for the prior 24 months given a fpa-fod fire event
-    # this will iteratively populate a list given each state grouping variable
-    fp_pop_den_summaries <- get_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 0) %>%
-      dplyr::select(-year_month_day)
+      # run get_climate_lags for the prior 24 months given a fpa-fod fire event
+      # this will iteratively populate a list given each state grouping variable
+      fp_pop_den_summaries <- get_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 0) %>%
+        dplyr::select(-year_month_day)
 
-    write_rds(fp_pop_den_summaries, file.path(per_state, paste0('pop_den_summary_', k, '.rds')))
+      write_rds(fp_pop_den_summaries, file.path(per_state, paste0('pop_den_summary_', k, '.rds')))
 
-    system(paste0("aws s3 sync ",
-                  summary_dir, " ",
-                  s3_proc_extractions))
-
+      system(paste0("aws s3 sync ",
+                    summary_dir, " ",
+                    s3_proc_extractions))
+    }
+    stopCluster(cl)
     return(fp_pop_den_summaries)
   }
-  stopCluster(cl)
   return(fp_pop_den_summaries)
 }
 
