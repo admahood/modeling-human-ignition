e883d2fff40df8ba18091aa17e7e6d7bb9eba174
‘admahood’ <admahood@gmail.com>
2018-03-20 21:57:18 +0000

    new approach to extract

--- src/R/c_data_prep_landcover_to_fpa.R
+++ src/R/c_data_prep_landcover_to_fpa.R
@@ -11,29 +11,63 @@ getmode <- function(v) {
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-# test -----------------
-fpa_small <- fpa[1:1000,]
-t0 <- Sys.time()
-fpa_small$lf <- raster::extract(landfire, fpa_small, buffer = 1000, fun = function(x,...)getmode(x))
-print(Sys.time()-t0)
-
-t0 <- Sys.time()
-beginCluster()
-fpa_small$lf <- raster::extract(landfire, fpa_small, buffer = 1000, fun = function(x,...)getmode(x))
-endCluster()
-print(Sys.time()-t0)
-fpa_small
-
-
-
-
-# we're doing it
-beginCluster(detectCores())
-fpa$landfire <- raster::extract(landfire, fpa, buffer = 1000, fun = function(x,...)getmode(x))
-endCluster()
-
-# trying out the raster parallel thing
+# # test runs-----------------
+# fpa_small <- fpa[1:1000,]
+# 
+# t0 <- Sys.time()
+# fpa_small$lf <- raster::extract(landfire, fpa_small, buffer = 1000, fun = function(x,...)getmode(x))
+# print(Sys.time()-t0) # 20 minutes 
+# 
+# t0 <- Sys.time()
 # beginCluster()
-# fpa$landfire <- clusterR(landfire, extract, args = list(y=fpa, buffer = 1000, fun=function(x,...)getmode(x)))
+# fpa_small$lf <- raster::extract(landfire, fpa_small, buffer = 1000, fun = function(x,...)getmode(x))
 # endCluster()
-# doesn't seem to work
+# print(Sys.time()-t0) #
+# fpa_small
+# 
+# # we're doing it for real --------------- but it's going to take 12.5 days so maybe  not
+# beginCluster(detectCores())
+# fpa$landfire <- raster::extract(landfire, fpa, buffer = 1000, fun = function(x,...)getmode(x))
+# endCluster()
+
+
+# new approach - same as lyb baecv
+corz <- detectCores()
+fpa <- st_transform(fpa, crs = crs(landfire, asText=TRUE))
+
+if (!exists("sp_grd")){
+  pol <- st_as_sfc(st_bbox(landfire))
+  grd <- st_make_grid(pol,n=c(corz,1))
+  sp_grd <- sf::as_Spatial(grd)
+}
+
+t1 <- Sys.time()
+registerDoParallel(cores=corz)
+splits <- foreach(j=1:length(sp_grd)) %dopar% {raster::crop(landfire, sp_grd[j])}
+print("time for splitting raster")
+print(Sys.time() - t1)
+#rm(landfire)
+
+## also need to split the fpa data
+t1 <- Sys.time()
+registerDoParallel(cores=corz)
+pnts <- foreach(j=1:length(sp_grd)) %dopar% {st_intersection(fpa, grd[j])} # gotta use the sf grid
+print("time for splitting fpa")
+print(Sys.time() - t1)
+
+
+spl_rcl <- list() #possibly unnecesary
+t1 <- Sys.time()
+registerDoParallel(cores=corz)
+print(paste("reclassifying"))
+
+spl_rcl <- foreach(k=1:length(splits)) %dopar% {
+  # making a filename based on location
+  #xmin <- (substr(as.character(sp_grd[k]@bbox[[1]]),1,4))
+  #filename <- paste0("scrap/rcl", year,"_", xmin, ".tif")
+  # applying the function
+  spl_rcl[[k]] <- raster::extract(splits[[k]], pnts[[k]], buffer=1000, fun = function(x,...)getmode(x))
+}
+print("time for reclassifying")
+print(Sys.time()-t1)
+rm(splits)
