00796d35109d6e8dbf7d00ae6e9d72d5d7c98467
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-03-21 16:31:04 -0600

    added tertiary and cleaned asthetics

--- src/R/c_data_prep_transportation_density.R
+++ src/R/c_data_prep_transportation_density.R
@@ -1,56 +1,63 @@
 
 unique_states <- unique(hexnet_4k$STUSPS)
 
-grid_list <- split_fast_tibble(hexnet_4k, hexnet_4k$STUSPS)
-line_list <- split_fast_tibble(rail_rds, rail_rds$STUSPS)
+hex_list <- split_fast_tibble(hexnet_4k, hexnet_4k$STUSPS)
 
-railroad_density <- get_density(unique_states, grid_list, line_list)
+if (!file.exists(file.path(transportation_density_dir, "railroad_density.gpkg"))) {
 
-st_write(railroad_density, file.path(transportation_density_dir, "railroad_density.gpkg"))
+  rail_list <- split_fast_tibble(rail_rds, rail_rds$STUSPS)
 
-system(paste0("aws s3 sync ",
-              processed_dir, " ",
-              s3_proc_prefix))
+  railroad_density <- get_density(unique_states, hex_list, rail_list, ncores = detectCores())
 
+  st_write(railroad_density, file.path(transportation_density_dir, "railroad_density.gpkg"))
 
-line_list <- split_fast_tibble(tl, tl$STUSPS)
+  system(paste0("aws s3 sync ", processed_dir, " ", s3_proc_prefix))
 
-transmission_lines_density <- get_density(unique_states, grid_list, line_list)
+}
 
-st_write(transmission_lines_density, file.path(transportation_density_dir, "transmission_lines_density.gpkg"))
+if (!file.exists(file.path(transportation_density_dir, "transmission_lines_density.gpkg"))) {
 
-system(paste0("aws s3 sync ",
-              processed_dir, " ",
-              s3_proc_prefix))
+  transmission_lines_list <- split_fast_tibble(tl, tl$STUSPS)
 
-line_list <- split_fast_tibble(primary_rds, primary_rds$STUSPS)
+  transmission_lines_density <- get_density(unique_states, hex_list, transmission_lines_list, ncores = detectCores())
 
-primary_rds_density <- get_density(unique_states, grid_list, line_list)
+  st_write(transmission_lines_density, file.path(transportation_density_dir, "transmission_lines_density.gpkg"))
 
-st_write(primary_rds_density, file.path(transportation_density_dir, "primary_rds_density.gpkg"))
+  system(paste0("aws s3 sync ", processed_dir, " ", s3_proc_prefix))
 
-system(paste0("aws s3 sync ",
-              processed_dir, " ",
-              s3_proc_prefix))
+}
 
+if (!file.exists(file.path(transportation_density_dir, "primary_rds_density.gpkg"))) {
 
-line_list <- split_fast_tibble(secondary_rds, secondary_rds$STUSPS)
+  primary_rds_list <- split_fast_tibble(primary_rds, primary_rds$STUSPS)
 
-secondary_rds_density <- get_density(unique_states, grid_list, line_list)
+  primary_rds_density <- get_density(unique_states, hex_list, primary_rds_list, ncores = detectCores())
 
-st_write(secondary_rds_density, file.path(transportation_density_dir, "secondary_rds_density.gpkg"))
+  st_write(primary_rds_density, file.path(transportation_density_dir, "primary_rds_density.gpkg"))
 
-system(paste0("aws s3 sync ",
-              processed_dir, " ",
-              s3_proc_prefix))
+  system(paste0("aws s3 sync ", processed_dir, " ", s3_proc_prefix))
 
+}
 
-line_list <- split_fast_tibble(tertiary_rds, tertiary_rds$STUSPS)
+if (!file.exists(file.path(transportation_density_dir, "secondary_rds_density.gpkg"))) {
 
-tertiary_rds_density <- get_density(unique_states, grid_list, line_list)
+  secondary_rds_list <- split_fast_tibble(secondary_rds, secondary_rds$STUSPS)
 
-st_write(tertiary_rds_density, file.path(transportation_density_dir, "tertiary_rds_density.gpkg"))
+  secondary_rds_density <- get_density(unique_states, hex_list, secondary_rds_list, ncores = detectCores())
 
-system(paste0("aws s3 sync ",
-              processed_dir, " ",
-              s3_proc_prefix))
+  st_write(secondary_rds_density, file.path(transportation_density_dir, "secondary_rds_density.gpkg"))
+
+  system(paste0("aws s3 sync ", processed_dir, " ", s3_proc_prefix))
+
+}
+
+if (!file.exists(file.path(transportation_density_dir, "tertiary_rds_density.gpkg"))) {
+
+  tertiary_rds_list <- split_fast_tibble(tertiary_rds, tertiary_rds$STUSPS)
+
+  tertiary_rds_density <- get_density(unique_states, hex_list, tertiary_rds_list, ncores = 16)
+
+  st_write(tertiary_rds_density, file.path(transportation_density_dir, "tertiary_rds_density.gpkg"))
+
+  system(paste0("aws s3 sync ", processed_dir, " ", s3_proc_prefix))
+}
