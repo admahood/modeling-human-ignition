3827bc282dff94408f47b40f4c3f14e40d54c914
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-04-02 21:27:02 +0000

    changed all transportation density function to use lapply

--- src/R/c_data_prep_transportation_density.R
+++ src/R/c_data_prep_transportation_density.R
@@ -1,10 +1,72 @@
+num_cores <- parallel::detectCores()
 
+hexnet_4k <- hexnet_4k %>%
+  mutate(sampled = sample(1:num_cores, nrow(.), replace = TRUE))
+
+hexnet_list <- hexnet_4k %>%
+  split(., .$STUSPS)
 
 if (!file.exists(file.path(transportation_density_dir, "railroad_density.gpkg"))) {
-
-  rail_list <- split_fast_tibble(rail_rds, rail_rds$STUSPS)
-
-  railroad_density <- get_density(unique_states, hex_list, rail_list, ncores = detectCores())
+  if (!exists("railroad_hex")) {
+    if (!file.exists(file.path(transportation_processed_dir, "railroad_hex.gpkg"))) {
+      
+      railroad_hex <- railroad %>%
+        dplyr::select(LINEARID, STUSPS) %>%
+        st_join(., hexnet_4k, join = st_intersects) %>%
+        mutate(STUSPS = STUSPS.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS)
+      
+      sf::st_write(railroad_hex,
+                   file.path(transportation_processed_dir, "railroad_hex.gpkg"),
+                   driver = "GPKG")
+      
+      system(paste0("aws s3 sync ",
+                    transportation_processed_dir, " ",
+                    s3_proc_prefix, "transportation/processed"))
+    } else {
+      
+      railroad_hex <- sf::st_read(dsn = file.path(transportation_processed_dir, "railroad_hex.gpkg")) %>%
+        as.data.frame() %>%
+        left_join(., as.data.frame(hexnet_4k), by = 'hexid4k') %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS.x, geom.x, sampled) %>%
+        mutate(STUSPS = STUSPS.x,
+               geom = geom.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS, sampled, geom) %>%
+        st_as_sf()
+      
+    }
+  }
+  
+  sfInit(parallel = TRUE, cpus = num_cores)
+  sfExport('railroad_hex')
+  sfSource('src/functions/helper_functions.R')
+  
+  railroad_density <- lapply(hexnet_list,
+                                       function (input_list) {
+                                         require(tidyverse)
+                                         require(magrittr)
+                                         require(lubridate)
+                                         require(lubridate)
+                                         require(sf)
+                                         
+                                         sub_grid <- dplyr:::bind_cols(input_list)
+                                         unique_ids <- unique(sub_grid$hexid4k)
+                                         state_name <- unique(sub_grid$STUSPS)[1]
+                                         
+                                         print(paste0('Working on ', state_name))
+                                         got_density <- lapply(unique_ids,
+                                                               FUN = get_density,
+                                                               grids = sub_grid,
+                                                               lines = railroad_hex)
+                                         print(paste0('Finishing ', state_name))
+                                         
+                                         return(got_density)
+                                       }
+  )
+  sfStop()
+  
+  railroad_density <- flattenlist(railroad_density) %>%
+    do.call(rbind, .)
 
   st_write(railroad_density, file.path(transportation_density_dir, "railroad_density.gpkg"))
 
@@ -13,10 +75,66 @@ if (!file.exists(file.path(transportation_density_dir, "railroad_density.gpkg"))
 }
 
 if (!file.exists(file.path(transportation_density_dir, "transmission_lines_density.gpkg"))) {
-
-  transmission_lines_list <- split_fast_tibble(tl, tl$STUSPS)
-
-  transmission_lines_density <- get_density(unique_states, hex_list, transmission_lines_list, ncores = detectCores())
+  if (!exists("transmission_lines_hex")) {
+    if (!file.exists(file.path(transportation_processed_dir, "transmission_lines_hex.gpkg"))) {
+      
+      transmission_lines_hex <- transmission_lines %>%
+        dplyr::select(LINEARID, STUSPS) %>%
+        st_join(., hexnet_4k, join = st_intersects) %>%
+        mutate(STUSPS = STUSPS.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS)
+      
+      sf::st_write(transmission_lines_hex,
+                   file.path(transportation_processed_dir, "transmission_lines_hex.gpkg"),
+                   driver = "GPKG")
+      
+      system(paste0("aws s3 sync ",
+                    transportation_processed_dir, " ",
+                    s3_proc_prefix, "transportation/processed"))
+    } else {
+      
+      transmission_lines_hex <- sf::st_read(dsn = file.path(transportation_processed_dir, "transmission_lines_hex.gpkg")) %>%
+        as.data.frame() %>%
+        left_join(., as.data.frame(hexnet_4k), by = 'hexid4k') %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS.x, geom.x, sampled) %>%
+        mutate(STUSPS = STUSPS.x,
+               geom = geom.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS, sampled, geom) %>%
+        st_as_sf()
+      
+    }
+  }
+  
+  sfInit(parallel = TRUE, cpus = num_cores)
+  sfExport('transmission_lines_hex')
+  sfSource('src/functions/helper_functions.R')
+  
+  transmission_lines_density <- lapply(hexnet_list,
+                                function (input_list) {
+                                  require(tidyverse)
+                                  require(magrittr)
+                                  require(lubridate)
+                                  require(lubridate)
+                                  require(sf)
+                                  
+                                  sub_grid <- dplyr:::bind_cols(input_list)
+                                  unique_ids <- unique(sub_grid$hexid4k)
+                                  state_name <- unique(sub_grid$STUSPS)[1]
+                                  
+                                  print(paste0('Working on ', state_name))
+                                  got_density <- lapply(unique_ids,
+                                                        FUN = get_density,
+                                                        grids = sub_grid,
+                                                        lines = transmission_lines_hex)
+                                  print(paste0('Finishing ', state_name))
+                                  
+                                  return(got_density)
+                                }
+  )
+  sfStop()
+  
+  transmission_lines_density <- flattenlist(transmission_lines_density) %>%
+    do.call(rbind, .)
 
   st_write(transmission_lines_density, file.path(transportation_density_dir, "transmission_lines_density.gpkg"))
 
@@ -25,10 +143,67 @@ if (!file.exists(file.path(transportation_density_dir, "transmission_lines_densi
 }
 
 if (!file.exists(file.path(transportation_density_dir, "primary_rds_density.gpkg"))) {
-
-  primary_rds_list <- split_fast_tibble(primary_rds, primary_rds$STUSPS)
-
-  primary_rds_density <- get_density(unique_states, hex_list, primary_rds_list, ncores = detectCores())
+  
+  if (!exists("primary_hex")) {
+    if (!file.exists(file.path(transportation_processed_dir, "primary_rds_hex.gpkg"))) {
+      
+      primary_hex <- primary_rds %>%
+        dplyr::select(LINEARID, STUSPS) %>%
+        st_join(., hexnet_4k, join = st_intersects) %>%
+        mutate(STUSPS = STUSPS.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS)
+      
+      sf::st_write(primary_hex,
+                   file.path(transportation_processed_dir, "primary_rds_hex.gpkg"),
+                   driver = "GPKG")
+      
+      system(paste0("aws s3 sync ",
+                    transportation_processed_dir, " ",
+                    s3_proc_prefix, "transportation/processed"))
+    } else {
+      
+      primary_hex <- sf::st_read(dsn = file.path(transportation_processed_dir, "primary_rds_hex.gpkg")) %>%
+        as.data.frame() %>%
+        left_join(., as.data.frame(hexnet_4k), by = 'hexid4k') %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS.x, geom.x, sampled) %>%
+        mutate(STUSPS = STUSPS.x,
+               geom = geom.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS, sampled, geom) %>%
+        st_as_sf()
+      
+    }
+  }
+  
+  sfInit(parallel = TRUE, cpus = num_cores)
+  sfExport('primary_hex')
+  sfSource('src/functions/helper_functions.R')
+  
+  primary_rds_density <- lapply(hexnet_list,
+                                 function (input_list) {
+                                   require(tidyverse)
+                                   require(magrittr)
+                                   require(lubridate)
+                                   require(lubridate)
+                                   require(sf)
+                                   
+                                   sub_grid <- dplyr:::bind_cols(input_list)
+                                   unique_ids <- unique(sub_grid$hexid4k)
+                                   state_name <- unique(sub_grid$STUSPS)[1]
+                                   
+                                   print(paste0('Working on ', state_name))
+                                   got_density <- lapply(unique_ids,
+                                                         FUN = get_density,
+                                                         grids = sub_grid,
+                                                         lines = primary_hex)
+                                   print(paste0('Finishing ', state_name))
+                                   
+                                   return(got_density)
+                                   }
+                                 )
+  sfStop()
+  
+  primary_rds_density <- flattenlist(primary_rds_density) %>%
+    do.call(rbind, .)
 
   st_write(primary_rds_density, file.path(transportation_density_dir, "primary_rds_density.gpkg"))
 
@@ -37,10 +212,66 @@ if (!file.exists(file.path(transportation_density_dir, "primary_rds_density.gpkg
 }
 
 if (!file.exists(file.path(transportation_density_dir, "secondary_rds_density.gpkg"))) {
-
-  secondary_rds_list <- split_fast_tibble(secondary_rds, secondary_rds$STUSPS)
-
-  secondary_rds_density <- get_density(unique_states, hex_list, secondary_rds_list, ncores = detectCores())
+  if (!exists("secondary_hex")) {
+    if (!file.exists(file.path(transportation_processed_dir, "secondary_rds_hex.gpkg"))) {
+      
+      secondary_hex <- secondary_rds %>%
+        dplyr::select(LINEARID, STUSPS) %>%
+        st_join(., hexnet_4k, join = st_intersects) %>%
+        mutate(STUSPS = STUSPS.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS)
+      
+      sf::st_write(secondary_hex,
+                   file.path(transportation_processed_dir, "secondary_rds_hex.gpkg"),
+                   driver = "GPKG")
+      
+      system(paste0("aws s3 sync ",
+                    transportation_processed_dir, " ",
+                    s3_proc_prefix, "transportation/processed"))
+    } else {
+      
+      secondary_hex <- sf::st_read(dsn = file.path(transportation_processed_dir, "secondary_rds_hex.gpkg")) %>%
+        as.data.frame() %>%
+        left_join(., as.data.frame(hexnet_4k), by = 'hexid4k') %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS.x, geom.x, sampled) %>%
+        mutate(STUSPS = STUSPS.x,
+               geom = geom.x) %>%
+        dplyr::select(LINEARID, hexid4k, STUSPS, sampled, geom) %>%
+        st_as_sf()
+      
+    }
+  }
+  
+  sfInit(parallel = TRUE, cpus = num_cores)
+  sfExport('secondary_hex')
+  sfSource('src/functions/helper_functions.R')
+  
+  secondary_rds_density <- lapply(hexnet_list,
+                                function (input_list) {
+                                  require(tidyverse)
+                                  require(magrittr)
+                                  require(lubridate)
+                                  require(lubridate)
+                                  require(sf)
+                                  
+                                  sub_grid <- dplyr:::bind_cols(input_list)
+                                  unique_ids <- unique(sub_grid$hexid4k)
+                                  state_name <- unique(sub_grid$STUSPS)[1]
+                                  
+                                  print(paste0('Working on ', state_name))
+                                  got_density <- lapply(unique_ids,
+                                                        FUN = get_density,
+                                                        grids = sub_grid,
+                                                        lines = secondary_hex)
+                                  print(paste0('Finishing ', state_name))
+                                  
+                                  return(got_density)
+                                }
+  )
+  sfStop()
+  
+  secondary_rds_density <- flattenlist(secondary_rds_density) %>%
+    do.call(rbind, .)
 
   st_write(secondary_rds_density, file.path(transportation_density_dir, "secondary_rds_density.gpkg"))
 
@@ -49,10 +280,6 @@ if (!file.exists(file.path(transportation_density_dir, "secondary_rds_density.gp
 }
 
 if (!file.exists(file.path(transportation_density_dir, "tertiary_rds_density.gpkg"))) {
-  num_cores <- 4 #parallel::detectCores()
-
-  hexnet_4k <- hexnet_4k %>%
-    mutate(sampled = sample(1:num_cores, nrow(.), replace = TRUE))
 
   if (!exists("tertiary_hex")) {
     if (!file.exists(file.path(transportation_processed_dir, "tertiary_rds_hex.gpkg"))) {
@@ -72,25 +299,9 @@ if (!file.exists(file.path(transportation_density_dir, "tertiary_rds_density.gpk
                     s3_proc_prefix, "transportation/processed"))
     } else {
 
-      tertiary_hex <- sf::st_read(dsn = file.path(transportation_processed_dir, "tertiary_rds_hex.gpkg")) # %>%
-        # as.data.frame() %>%
-        # dplyr::select(-sampled) %>%
-        # left_join(., as.data.frame(hexnet_4k), by = 'hexid4k') %>%
-        # dplyr::select(LINEARID, hexid4k, STUSPS.x, geom.x, sampled) %>%
-        # mutate(STUSPS = STUSPS.x,
-        #        geom = geom.x) %>%
-        # dplyr::select(LINEARID, hexid4k, STUSPS, sampled, geom) %>%
-        # st_as_sf()
-
+      tertiary_hex <- sf::st_read(dsn = file.path(transportation_processed_dir, "tertiary_rds_hex.gpkg"))
     }
   }
-
-  hexnet_list <- hexnet_4k %>%
-    split(., .$STUSPS)
-
-  # sfInit(parallel = TRUE, cpus = num_cores)
-  # sfExport('tertiary_hex')
-  # sfSource('src/functions/helper_functions.R')
 
   tertiary_rds_density <- lapply(hexnet_list,
     function (input_list) {
@@ -127,15 +338,15 @@ if (!file.exists(file.path(transportation_density_dir, "tertiary_rds_density.gpk
       return(got_density)
     }
     )
+  
+  per_state_list <- list.files(per_state, full.names = TRUE)
 
-  # sfStop()
-
-
-
-  extraction_df <- flattenlist(tertiary_rds_density) %>%
-    do.call(rbind, .)
-
-  tertiary_rds_density <- get_density(unique_states, hex_list, tertiary_rds_list, ncores = 16)
+  tertiary_per_state <- lapply(per_state_list, function(x) st_read(x))
+  
+  tertiary_rds_density <- flattenlist(tertiary_per_state) %>%
+    do.call(rbind, .) %>%
+    mutate(length_line = if_else(is.na(length_line), 0, length_line),
+           density = if_else(is.na(density), 0, density))
 
   st_write(tertiary_rds_density, file.path(transportation_density_dir, "tertiary_rds_density.gpkg"))
 
