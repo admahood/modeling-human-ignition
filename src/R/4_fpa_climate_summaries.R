3310c3f35d348210c3693ca776f72280d7b9a442
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-05-10 14:48:39 -0600

    working

--- src/R/4_fpa_climate_summaries.R
+++ src/R/4_fpa_climate_summaries.R
@@ -1,4 +1,3 @@
-
 if (!exists("fpa_ll")) {
   if (file.exists(file.path(processed_dir, "fpa_ll.gpkg"))) {
 
@@ -36,12 +35,10 @@ vars <- c('aet', 'def', 'ffwi', 'fm100', 'pdsi', 'pr', 'tmmx', 'vpd', 'vs')
 
 unique_states <- unique(fpa_ll$STATE)
 
-sub_fpa <- fpa_ll %>%
-  select(FPA_ID, STATE, year_month_day)
-
 for (j in stat){
   for (i in vars) {
     fpa_summaries <- list()
+    counter <- 1
 
     tifs <- check_tifs(j, i)
 
@@ -49,9 +46,16 @@ for (j in stat){
 
       print(paste0('Skipping ', j, ' ', i))
 
-    } else {
+    } else if (!file.exists(file.path(summaries_dir, j, paste0('fpa_', i, '_', j, '_summaries.rds')))) {
 
-      if (!file.exists(file.path(climate_prefix, j, paste0('fpa_', i, '_', j, '_extraction.rds')))) {
+      # subset the fpa-fod data based on state grouping variable
+      # this increases the speed of this function and only needs ~40GB of memory max.
+      for (k in 1:length(unique_states)) {
+
+        # create a subdataframe based on state subset
+        sub_df <- subset(fpa_ll, fpa_ll$STATE == unique_states[k])
+
+        print(paste0('Working on ', counter, " of ", length(unique_states), " for the ", j, ' ', i))
 
         # setup parallel environment
         cl <- makeCluster(detectCores())
@@ -59,83 +63,65 @@ for (j in stat){
 
         # extract climate time series data based on point or polygon locations.
         # this extract is pulling in point data, so fun = mean does not matter
-        print(paste0('Parallel ', j, ' ', i))
+        extractions <- foreach (i = tifs) %dopar% {
 
-        extractions <- foreach (l = tifs) %dopar% {
-
-          res <- extract_one(l, shapefile_extractor = sub_fpa)
-
+          res <- raster::extract(raster::stack(i), sub_df,
+                                 na.rm = TRUE, fun = 'mean', df = TRUE)
         }
+        system('aws s3 sync data/climate s3://earthlab-modeling-human-ignitions/climate')
+
         stopCluster(cl)
 
-        extraction_df <- extractions %>%
-          bind_cols %>%
-          as_tibble %>%
-          mutate(index = ID) %>%
-          select(-starts_with("ID")) %>%
-          rename(ID = index) %>%
-          mutate(FPA_ID = data.frame(sub_fpa)$FPA_ID) %>%
-          dplyr::select(-starts_with('X')) %>%
-          left_join(., sub_fpa, by = 'FPA_ID')
+        # ensure that they all have the same length
+        stopifnot(all(lapply(extractions, nrow) == nrow(sub_df)))
 
-        write_rds(extraction_df, file.path(climate_prefix, j, paste0('fpa_', i, '_', j, '_extraction.rds')))
-        system(paste0("aws s3 sync ", climate_prefix, " ", s3_proc_climate))
+        if (!file.exists(file.path(summaries_dir, j, 'state', paste0('fpa_', k, '_', i, '_', j, '_summaries.rds')))) {
+
+          # convert to a data frame
+          extraction_df <- extractions %>%
+            bind_cols %>%
+            as_tibble %>%
+            mutate(index = ID) %>%
+            select(-starts_with("ID")) %>%
+            rename(ID = index) %>%
+            mutate(FPA_ID = data.frame(sub_df)$FPA_ID) %>%
+            dplyr::select(-starts_with('X')) %>%
+            gather(variable, value, -FPA_ID, -ID) %>%
+            filter(!is.na(value)) %>%
+            mutate(FPA_ID = as.factor(FPA_ID),
+                   ID = as.factor(ID)) %>%
+            # clean the final, long climate data frame with linked fpa ids
+            separate(variable,
+                     into = c("variable", 'year', "statistic", "month"),
+                     sep = "_|\\.") %>%
+            mutate(day = '01',
+                   year_month_day = as.Date(paste(year, month, day, sep='-')))
+
+          # reduce the size of the dataframe to be joined during the get_climate_lags
+          sub_df <- sub_df %>%
+            dplyr::select(FPA_ID, year_month_day)
+
+          # run get_climate_lags for the prior 24 months given a fpa-fod fire event
+          # this will iteratively populate a list given each state grouping variable
+          fpa_summaries_out <- get_lags(sub_df, extraction_df, sub_df$year_month_day, time_lag = 24) %>%
+            dplyr::select(-year_month_day)
+
+          write_rds(fpa_summaries_out, file.path(summaries_dir, j, 'state', paste0('fpa_', k, '_', i, '_', j, '_summaries.rds')))
+          system('aws s3 sync data/extractions s3://earthlab-modeling-human-ignitions/extractions')
+
+          fpa_summaries[[k]] <- fpa_summaries_out
+          counter <- counter + 1
+        } else {
+          fpa_summaries_out <- read_rds(file.path(summaries_dir, j, 'state', paste0('fpa_', k, '_', i, '_', j, '_summaries.rds')))
+
+          fpa_summaries[[k]] <- fpa_summaries_out
+          counter <- counter + 1
+        }
 
       }
-
-      if (!file.exists(file.path(summaries_dir, j, paste0('fpa_', i, '_', j, '_summaries.rds')))) {
-
-        extraction_df <- read_rds(file.path(climate_prefix, j, paste0('fpa_', i, '_', j, '_extraction.rds')))
-
-        extraction_mini <- extraction_df %>%
-          slice(1:10)
-        print(paste0('Creating extraction tibble for ', j, ' ', i))
-
-        # subset the fpa-fod data based on state grouping variable
-        # this increases the speed of this function and only needs ~40GB of memory max.
-
-        fpa_summaries <- for (k in 1:length(unique_states)) {
-        #fpa_summaries <- for (k in 1:length(unique_states)) {
-          require(tidyverse)
-
-          sub_extract <- extraction_mini %>%
-            filter(STATE == unique_states[k])
-
-          sub_extract$STATE <- droplevels(sub_extract$STATE)
-
-          unique_fpa_id <- unique(sub_extract$FPA_ID)
-
-          cl <- makeCluster(12)
-          registerDoParallel(cl)
-
-          fpa_summaries <-foreach(m = 1:length(unique_fpa_id), .combine = 'rbind') %dopar% {
-          #for (j in 1:length(unique_fpa_id)) {
-            require(tidyverse)
-
-            # create a subdataframe based on state subset
-            sub_extraction_df <- sub_extract %>%
-              filter(FPA_ID == unique_fpa_id[m])
-            sub_extraction_df$FPA_ID <- droplevels(sub_extraction_df$FPA_ID)
-
-            sub_extraction_df <- sub_extraction_df %>%
-              dplyr::select(-geom, -year_month_day, -ID, -STATE) %>%
-              gather(variable, value, -FPA_ID) %>%
-              mutate(FPA_ID = as.factor(FPA_ID)) %>%
-              separate(variable,
-                       into = c("variable", 'year', "statistic", "month"),
-                       sep = "_|\\.") %>%
-              mutate(day = '01',
-                     year_month_day = as.Date(paste(year, month, day, sep='-')))
-
-            fpa_summaries <- get_lags(sub_fpa, sub_extraction_df, sub_fpa$year_month_day, time_lag = 24) %>%
-              dplyr::select(-year_month_day)
-
-            fpa_summaries
-          }
-          stopCluster(cl)
-
-          fpa_summaries <- do.call(rbind, fpa_summaries) # Convert to data frame format
-        }
+      if (length(fpa_summaries) > 0) {
+        # rbinds the iteratively populated list and creates a cohesive dataframe
+        fpa_summaries <- do.call(rbind, fpa_summaries) # Convert to data frame format
 
         # save the final cleaned climate extractions joined with the fpa-fod database
         summary_name <- file.path(summaries_dir, j, paste0('fpa_', i, '_', j, '_summaries.rds'))
@@ -143,8 +129,10 @@ for (j in stat){
 
         # push to S3
         system('aws s3 sync data/extractions s3://earthlab-modeling-human-ignitions/extractions')
-
       }
     }
+
+    # creating the final dataframe takes about 150 GB of RAM, this clears the cache so it can be run again
+    gc()
   }
 }
